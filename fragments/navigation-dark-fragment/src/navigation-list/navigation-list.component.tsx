ce3a9661a39d9f327ab9fd3a37dcddeb81e8a1a3
Kirill Zolottsev <carmine@atls.team>
2022-07-06 14:58:38 +0400

    chore: link containers

--- fragments/navigation-dark-fragment/src/navigation-list/navigation-list.component.tsx
+++ fragments/navigation-dark-fragment/src/navigation-list/navigation-list.component.tsx
@@ -2,6 +2,7 @@ import React                   from 'react'
 import { FC }                  from 'react'
 
 import { Box }                 from '@ui/layout'
+import { Layout }              from '@ui/layout'
 import { NextNavLink }         from '@ui/link'
 
 import { NavigationItem }      from '../navigation-item'
@@ -9,15 +10,21 @@ import { NavigationListProps } from './navigation-list.interface'
 
 const NavigationList: FC<NavigationListProps> = ({ active }) => (
   <Box width='100%' justifyContent='space-between' flexWrap='wrap'>
-    <NextNavLink path='/services'>
-      <NavigationItem name='services' active={active} />
-    </NextNavLink>
-    <NextNavLink path='/contacts'>
-      <NavigationItem name='contacts' active={active} />
-    </NextNavLink>
-    <NextNavLink path='/blog'>
-      <NavigationItem name='blog' active={active} />
-    </NextNavLink>
+    <Layout>
+      <NextNavLink path='/services'>
+        <NavigationItem name='services' active={active} />
+      </NextNavLink>
+    </Layout>
+    <Layout>
+      <NextNavLink path='/contacts'>
+        <NavigationItem name='contacts' active={active} />
+      </NextNavLink>
+    </Layout>
+    <Layout>
+      <NextNavLink path='/blog'>
+        <NavigationItem name='blog' active={active} />
+      </NextNavLink>
+    </Layout>
   </Box>
 )
 
--- fragments/navigation-fragment/src/navigation-list/navigation-list.component.tsx
+++ fragments/navigation-fragment/src/navigation-list/navigation-list.component.tsx
@@ -2,20 +2,27 @@ import React           from 'react'
 import { FC }          from 'react'
 
 import { Box }         from '@ui/layout'
+import { Layout }      from '@ui/layout'
 import { NextNavLink } from '@ui/link'
 import { Text }        from '@ui/text'
 
 const NavigationList: FC = () => (
   <Box width='100%' justifyContent='space-between' flexWrap='wrap'>
-    <NextNavLink path='/services'>
-      <Text fontWeight='medium'>services</Text>
-    </NextNavLink>
-    <NextNavLink path='/contacts'>
-      <Text fontWeight='medium'>contacts</Text>
-    </NextNavLink>
-    <NextNavLink path='/blog'>
-      <Text fontWeight='medium'>blog</Text>
-    </NextNavLink>
+    <Layout>
+      <NextNavLink path='/services'>
+        <Text fontWeight='medium'>services</Text>
+      </NextNavLink>
+    </Layout>
+    <Layout>
+      <NextNavLink path='/contacts'>
+        <Text fontWeight='medium'>contacts</Text>
+      </NextNavLink>
+    </Layout>
+    <Layout>
+      <NextNavLink path='/blog'>
+        <Text fontWeight='medium'>blog</Text>
+      </NextNavLink>
+    </Layout>
   </Box>
 )
 
