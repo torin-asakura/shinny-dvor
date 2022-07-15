0c3ea3ee77329e18396476d360c16b7989f14e9a
Kirill Zolottsev <carmine@atls.team>
2022-07-15 18:15:04 +0400

    refactor(footer-fragment): update helper

--- fragments/footer-fragment/src/helpers/string-separator.helper.ts
+++ fragments/footer-fragment/src/helpers/string-separator.helper.ts
@@ -1,12 +1,12 @@
 const stringSeparator = (string: string) => {
   const buffer = string?.replace(/(<p>|<\/p>)/g, '').replace(/(&#8211;)/g, '-')
 
-  const firstPart = buffer
-  const secondPart = buffer
+  const firstPart = buffer?.substring(0, buffer.indexOf('<br>'))
+  const secondPart = buffer?.slice(buffer.indexOf('<br>'))
 
   return {
-    firstPart: firstPart?.substring(0, firstPart.indexOf('<br>')),
-    secondPart: secondPart?.slice(secondPart.indexOf('<br>')),
+    firstPart,
+    secondPart,
   }
 }
 
