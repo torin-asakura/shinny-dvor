a0e7e565681b40837a5cd5b3a5f9276f49b4bd25
Kirill Zolottsev <carmine@atls.team>
2022-12-09 19:16:18 +0400

    chore(prices): logging

--- prices/service-entrypoint/src/http/fetch-data.ts
+++ prices/service-entrypoint/src/http/fetch-data.ts
@@ -1,12 +1,27 @@
-import fetch from 'node-fetch'
+import { Logger } from '@atls/logger'
 
-const fetchData = async (url: string) =>
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'x-client-key': `Application ${process.env.API_KEY}`,
-    },
-  })
+import fetch      from 'node-fetch'
+
+const logger = new Logger('Fetch-data')
+
+const fetchData = async (url: string) => {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'x-client-key': `Application ${process.env.API_KEY}`,
+      },
+    })
+
+    logger.info(`successful fetching: ${url}`)
+
+    return await response.json()
+  } catch (error) {
+    logger.error(error)
+  }
+
+  return null
+}
 
 export { fetchData }
