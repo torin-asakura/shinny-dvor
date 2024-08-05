67dafd03e4cfd5eab5df368100336e5b632a4213
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-05 16:47:13 +0300

    refactor(next.config): upgrade on site entrypoint

--- site/app/src/next.config.js
+++ site/app/src/next.config.js
@@ -1,16 +1,55 @@
-const { withSentryConfig } = require('@sentry/nextjs')
+import { withSentryConfig } from '@sentry/nextjs'
 
+import { join } from 'path'
+import { dirname } from 'path'
+
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
   experimental: {
     externalDir: true,
-    swcFileReading: false,
-    workerThreads: true,
+    outputFileTracingRoot: join(__dirname, './'),
     esmExternals: 'loose',
-    outputStandalone: true,
   },
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (webpackConfig, { webpack, isServer }) => {
+    // в названии пакета есть ".js", поэтому меняю регекс
+    webpackConfig.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(new RegExp(/^(\.{1,2}\/)+\S*\.js$/), function (
+        /** @type {{ request: string }} */
+        resource
+      ) {
+        resource.request = resource.request.replace('.js', '')
+      })
+    )
+
+    // discord.js not resolve zLib-sync without it
+    // https://github.com/discordjs/discord.js/issues/9592
+    webpackConfig.module.rules.push({
+      test: /\.node$/,
+      loader: 'node-loader',
+    })
+
+    return webpackConfig
+  },
+  output: 'standalone',
 }
 
-module.exports = withSentryConfig(
+export default withSentryConfig(
   nextConfig,
   {
     // For all available options, see:
