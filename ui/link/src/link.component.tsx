b71983b6680de6e7da692fea2d2454f7fced2100
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-07 11:59:29 +0300

    refactor(@ui/link): clean comments; remove unused imports

--- ui/link/src/link.component.tsx
+++ ui/link/src/link.component.tsx
@@ -2,14 +2,11 @@ import styled                      from '@emotion/styled'
 import { createBaseStyles }        from '@atls-ui-parts/link'
 
 import React                       from 'react'
-import { default as BaseNextLink } from 'next/link'
 
 import { Text }                    from '@ui/text'
 
-// import { createNextLink }          from '@atls-ui-parts/link'
-// import { createNextNavLink }       from '@atls-ui-parts/link'
-import { createNextLink }          from './create-next-link.hook.js'
-import { createNextNavLink }       from './create-next-nav-link.hook.js'
+import { createNextLink }          from './hooks/index.js'
+import { createNextNavLink }       from './hooks/index.js'
 import { activeNavLinkStyles }     from './next-nav-link.styles'
 import { appearanceNavLinkStyles } from './next-nav-link.styles'
 import { defaultNavLinkStyles }    from './next-nav-link.styles'
@@ -32,21 +29,11 @@ export const NavLink = styled(Link)(
 )
 
 export const NextLink = (props) => {
-  // TODO проверить.
-  // 	смотрю в навигации, она использует другой компонент
-  // найти компонент, который использует этот компонент и проверить
-
   const NextLinkRenderer = createNextLink(BaseLink)
   return <NextLinkRenderer {...props} />
-
-  // return <BaseNextLink {...props} href='' />
 }
 
-// TODO to use it with app router
-// needs to change 'next/router' to 'next/navigation' package
 export const NextNavLink = (props) => {
   const NextNavLinkRenderer = createNextNavLink(NavLink)
   return <NextNavLinkRenderer {...props} />
-
-  // return <BaseNextLink {...props} href='' />
 }
