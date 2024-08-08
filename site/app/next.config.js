6aa84951b375aa5f049e3291b5fc8247b007b15d
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-08 19:02:10 +0300

    refacotr(site): prettier

--- site/app/next.config.js
+++ site/app/next.config.js
@@ -2,7 +2,6 @@ import { withSentryConfig } from '@sentry/nextjs'
 
 import { join } from 'path'
 import { dirname } from 'path'
-
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
