236046451130e0a3e1b66a64cca4080da067ac57
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-12 18:44:21 +0300

    refactor(blog/index-page): props destructure rewrite

--- blog/pages/index-page/src/index-page.client.tsx
+++ blog/pages/index-page/src/index-page.client.tsx
@@ -1,39 +1,34 @@
 // move it directive to fragment level
 'use client'
 
-import { AllArticles } from '@blog/articles-fragment'
-import { Footer }      from '@blog/footer-fragment'
-import { Navigation }  from '@blog/navigation-fragment'
+import React              from 'react'
+import { FC }             from 'react'
 
-import React           from 'react'
-import { FC }          from 'react'
+import { AllArticles }    from '@blog/articles-fragment'
+import { Footer }         from '@blog/footer-fragment'
+import { Navigation }     from '@blog/navigation-fragment'
+import { Column }         from '@ui/layout'
 
-import { Column }      from '@ui/layout'
+import { IndexPageProps } from './index-page.interfaces.js'
+import { Seo }            from './seo.component.js'
 
-import { Seo }         from './seo.component.js'
+export const IndexPageClient: FC<IndexPageProps> = (props) => {
+  const { ogCover, SEO, data } = props
 
-interface Props {
-  ogCover: string
-  SEO: any
-  data: any
+  const { contacts, posts, navigation, availableRadii, fragments, carBodies, services } = data
+
+  return (
+    <Column width='100%' alignItems='center'>
+      <Seo ogCover={ogCover} SEO={SEO} />
+      <Navigation
+        availableRadiiData={availableRadii}
+        navigationData={navigation}
+        fragmentsData={fragments}
+        carBodiesData={carBodies}
+        servicesData={services}
+      />
+      <AllArticles fragmentsData={fragments} postsData={posts} />
+      <Footer fragmentsData={fragments} contactsData={contacts} />
+    </Column>
+  )
 }
-
-// TODO interfaces
-export const IndexPageClient: FC<Props> = ({
-  ogCover,
-  SEO,
-  data: { contacts, posts, navigation, availableRadii, fragments, carBodies, services },
-}) => (
-  <Column width='100%' alignItems='center'>
-    <Seo ogCover={ogCover} SEO={SEO} />
-    <Navigation
-      availableRadiiData={availableRadii}
-      navigationData={navigation}
-      fragmentsData={fragments}
-      carBodiesData={carBodies}
-      servicesData={services}
-    />
-    <AllArticles fragmentsData={fragments} postsData={posts} />
-    <Footer fragmentsData={fragments} contactsData={contacts} />
-  </Column>
-)
