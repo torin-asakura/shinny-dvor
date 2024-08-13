6c0fc6b1492eb5e4463be2ac86471a9d3adb19b7
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-13 11:34:58 +0300

    refactor(site/index-page): separate login to use-hook

---
+++ site/pages/index-page/src/hooks/use-index-page-client.hook.ts
@@ -0,0 +1,38 @@
+import type { UseIndexPageClientType } from './use-index-page-client.interface.js'
+
+import { useEffect }                   from 'react'
+
+import { useIntersectionObserver }     from '@ui/intersection-observer'
+
+export const useIndexPageClient: UseIndexPageClientType = ({
+  isLoaded,
+  setActive,
+  headerRef,
+  setScrollY,
+}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      isLoaded.current = true
+    }, 200)
+  }, [])
+
+  const { getObserverOptions } = useIntersectionObserver((id) => {
+    const order = ['hero', 'services', 'articles', 'infographics', 'works-examples']
+    if (isLoaded.current) {
+      setActive(order.indexOf(id))
+    }
+  })
+
+  const scrollHandler = () => {
+    const y = headerRef!.current!.getBoundingClientRect()
+
+    setScrollY(y.y)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler, false)
+    return () => window.removeEventListener('scroll', scrollHandler, false)
+  }, [])
+
+  return { getObserverOptions }
+}
