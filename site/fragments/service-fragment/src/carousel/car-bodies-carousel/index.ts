0afc67402d2283371b94c33fad82de6ff4f53af0
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-21 17:20:08 +0300

    refactor(service-fragment/carousel): separate tablet&mobile component variants into individual components

---
+++ site/fragments/service-fragment/src/carousel/car-bodies-carousel/index.ts
@@ -0,0 +1,1 @@
+export * from './car-bodies-carousel.component.js'
---
+++ site/fragments/service-fragment/src/carousel/work-examples-carousel/work-examples-mobile-carousel/work-examples-mobile-carousel.component.tsx
@@ -0,0 +1,18 @@
+import type { FC }                from 'react'
+import type { PropsWithChildren } from 'react'
+
+import { useCarousel }            from '@ui/carousel'
+
+export const WorkExamplesMobileCarousel: FC<PropsWithChildren> = ({ children }) => {
+  const { carousel } = useCarousel({
+    children,
+    slidesPerView: 2,
+    spaceBetween: 260,
+    centered: true,
+    height: 282,
+    width: 340,
+    loop: false,
+  })
+
+  return carousel
+}
