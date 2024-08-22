9e0170e8704a5f0fdaa0b55352191890ef265801
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-22 12:05:37 +0300

    refactor: chagne if-elste to swith loop

--- fragments/navigation-fragment/src/helpers/get-color-background.ts
+++ fragments/navigation-fragment/src/helpers/get-color-background.ts
@@ -1,8 +1,16 @@
 export const getColorBackground = (step: number, scrollY?: number) => {
-  if (step === 0 && Math.abs(scrollY!) > 100) return 'white'
-  if (step === 1) return 'white'
-  if (step === 2) return 'white'
-  if (step === 3) return 'white'
-  if (step === 4) return 'white'
-  return 'transparent'
+  switch (step) {
+    case 0 && Math.abs(scrollY!):
+      return 'wite'
+    case 1:
+      return 'white'
+    case 2:
+      return 'white'
+    case 3:
+      return 'white'
+    case 4:
+      return 'white'
+    default:
+      return 'transparent'
+  }
 }
