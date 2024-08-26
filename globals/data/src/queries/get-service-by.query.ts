7d94b7743ea16955cdb7b9014d679ed1264edbd7
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-26 11:57:27 +0300

    refactor(globals/query): change function in query string to fixed props

--- globals/data/src/queries/get-service-by.query.ts
+++ globals/data/src/queries/get-service-by.query.ts
@@ -1,47 +1,116 @@
-import { gql }      from '@apollo/client'
+import { gql } from '@apollo/client'
 
-import { getRadii } from '@globals/data'
+// TODO refactor prices
 
 const GET_SERVICE_BY = gql`
-        query GetServiceBy($uri: String!) {
-            serviceBy(uri: $uri) {
-                servicesParams {
-                    title
-                    description
-                    fragmentId
-                    addon
-                    variant
-                    bodies
-                    image {
-                        altText
-                        sourceUrl
-                    }
-                    workexamples {
-                        firstexample {
-                            title
-                            image {
-                                altText
-                                sourceUrl
-                            }
-                        }
-                        secondexample {
-                            title
-                            image {
-                                altText
-                                sourceUrl
-                            }
-                        }
-                    }
-                    price {
-                        ${await getRadii()}
-                    }
-                    additionalservice {
-                        title
-                        price
-                    }
-                }
-            }
+  query GetServiceBy($uri: String!) {
+    serviceBy(uri: $uri) {
+      servicesParams {
+        title
+        description
+        fragmentId
+        addon
+        variant
+        bodies
+        image {
+          altText
+          sourceUrl
         }
+        workexamples {
+          firstexample {
+            title
+            image {
+              altText
+              sourceUrl
+            }
+          }
+          secondexample {
+            title
+            image {
+              altText
+              sourceUrl
+            }
+          }
+        }
+        price {
+          r13 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r14 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r15 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r16 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r17 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r18 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r19 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r20 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r21 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+          r22 {
+            commercial
+            jeep
+            minibus
+            minivan
+            passenger
+          }
+        }
+        additionalservice {
+          title
+          price
+        }
+      }
+    }
+  }
 `
 
 export { GET_SERVICE_BY }
