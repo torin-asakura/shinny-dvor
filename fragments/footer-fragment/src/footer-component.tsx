9f5f3a2389ef4e9aab4fb8309bf2ddf617d2a788
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-26 11:57:59 +0300

    format

--- fragments/footer-fragment/src/footer-component.tsx
+++ fragments/footer-fragment/src/footer-component.tsx
@@ -1,9 +1,8 @@
+import type { FooterProps } from './footer.interface.js'
 import type { FC }          from 'react'
 
-import type { FooterProps } from './footer.interface.js'
-
-import { memo }             from 'react'
 import React                from 'react'
+import { memo }             from 'react'
 
 import { Divider }          from '@ui/divider'
 import { Box }              from '@ui/layout'
