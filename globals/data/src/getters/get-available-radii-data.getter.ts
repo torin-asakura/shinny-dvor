58cff4140c36a70bd73d4d25986dc85774d55e80
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-27 12:35:14 +0300

    refactor(@globals/data): naming

--- globals/data/src/getters/get-available-radii-data.getter.ts
+++ globals/data/src/getters/get-available-radii-data.getter.ts
@@ -1,12 +1,12 @@
-import type { AvailableRadiiDataType } from '@globals/data'
+import type { AvailableRadiiQueryDataType } from '@globals/data'
 
-import { GET_AVAILABLE_RADII }         from '@globals/data'
-import { getClient }                   from '@globals/data'
+import { GET_AVAILABLE_RADII }              from '@globals/data'
+import { getClient }                        from '@globals/data'
 
 const getAvailableRadiiData = async () => {
   const client = getClient()
 
-  const { data }: { data: AvailableRadiiDataType } = await client.query({
+  const { data }: { data: AvailableRadiiQueryDataType } = await client.query({
     query: GET_AVAILABLE_RADII,
   })
 
