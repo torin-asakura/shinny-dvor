5a63b0c25e276fabc0724b64355f88d83d0e2dae
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-27 18:08:53 +0300

    chore: format

--- site/fragments/works-examples-fragment/src/works-examples.interface.ts
+++ site/fragments/works-examples-fragment/src/works-examples.interface.ts
@@ -1,6 +1,5 @@
 import type { FragmentsDataType }   from '@globals/data'
 import type { WorkResultsDataType } from '@globals/data'
-import type { NonNullable }         from '@globals/data'
 
 export interface WorksExamplesProps {
   fragmentsData: FragmentsDataType
