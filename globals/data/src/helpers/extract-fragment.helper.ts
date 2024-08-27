8302c014220e3e9be46ba968fb2f4433e63965d1
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-27 12:36:29 +0300

    chore(@globals/data/helpers): add interface WIP

--- globals/data/src/helpers/extract-fragment.helper.ts
+++ globals/data/src/helpers/extract-fragment.helper.ts
@@ -1,2 +1,21 @@
-export const extractFragment = (key: string, id: string, data: any[]) =>
-  data?.filter((fragment) => fragment[key].fragmentId === id)[0]?.[key]
+import type { FragmentsDataType }   from '@globals/data'
+import type { WorkResultsDataType } from '@globals/data'
+import type { ArrayElement }        from '@globals/data'
+import type { NonNullable }         from '@globals/data'
+
+type DataType = FragmentsDataType | WorkResultsDataType
+type GenericKeyType<T> = keyof NonNullable<ArrayElement<T>>
+// type KeyType = GenericKeyType<FragmentsDataType>
+
+type GenericExtractFragment<T> = (key: GenericKeyType<T>, id: string, data: T) => any
+
+type ExtractFragmentType =
+  | GenericExtractFragment<FragmentsDataType>
+  | GenericExtractFragment<WorkResultsDataType>
+
+const extractFragment: ExtractFragmentType = (key, id, data) =>
+  data?.filter((fragment) => {
+    return fragment[key]?.fragmentId === id
+  })[0]?.[key]
+
+export { extractFragment }
