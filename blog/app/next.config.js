b8896fccb86e41a7912027d99a72088cbe165fa1
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-28 13:45:33 +0300

    fix(@blog/app): \`next.config\` move from package to file; package json correct build scripts

--- blog/app/next.config.js
+++ blog/app/next.config.js
@@ -1,1 +1,33 @@
-export { nextConfig as default } from '@globals/configs'
+// export { nextConfig as default } from '@globals/configs'
+
+import { join } from 'path'
+import { dirname } from 'path'
+
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+/** @type {import('next').NextConfig} */
+export default {
+  experimental: {
+    externalDir: true,
+    outputFileTracingRoot: join(__dirname, './'),
+    esmExternals: 'loose',
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (webpackConfig, { webpack }) => {
+    webpackConfig.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(new RegExp(/\.js$/), function (
+        /** @type {{ request: string }} */
+        resource
+      ) {
+        resource.request = resource.request.replace('.js', '')
+      })
+    )
+    return webpackConfig
+  },
+  output: 'standalone',
+}
