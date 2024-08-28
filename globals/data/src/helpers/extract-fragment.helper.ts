2bd6a2cb8b17f72221f45297b54f0882a739fac0
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-28 15:39:48 +0300

    fix(@globals/data/helpers): back to old version

--- globals/data/src/helpers/extract-fragment.helper.ts
+++ globals/data/src/helpers/extract-fragment.helper.ts
@@ -1,16 +1,4 @@
-import type { ArrayElement } from '@globals/data'
-import type { NonNullable }  from '@globals/data'
-
-type GenericKeyType<T> = keyof NonNullable<ArrayElement<T>>
-
-function extractFragment<DataType>(key: GenericKeyType<DataType>, id: string, data: DataType) {
-  // @ts-expect-error prop not exist
-  const filtredItems = data?.filter((fragment) => {
-    if (fragment) return fragment[key]?.fragmentId === id
-    return null
-  })
-  if (filtredItems) return filtredItems[0]?.[key]
-  return null
-}
-
-export { extractFragment }
+// @ts-expect-error any type
+export const extractFragment = (key: string, id: string, data) =>
+  // @ts-expect-error any type
+  data?.filter((fragment) => fragment[key].fragmentId === id)[0]?.[key]
