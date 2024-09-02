abfaf07385b3fc96dfb3c9baf3c95f99342e5ed7
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-02 19:13:27 +0300

    refactor(`@globals/data`): comment old variant

--- globals/data/src/_interfaces/work-results.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { WorkReultsQueryDataType } from '@globals/data'
-
-type WorkReultsType = Exclude<WorkReultsQueryDataType['workResultItems'], null | undefined>
-export type WorkResultsDataType = WorkReultsType['nodes']
