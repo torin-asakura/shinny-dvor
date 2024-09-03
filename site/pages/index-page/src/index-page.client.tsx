257dadc0c6d5004f1cd5b396fb06297d853787bb
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-03 16:29:35 +0300

    refactor(`@site/index-page`): import updated `  return <>blabla</>`

--- site/pages/index-page/src/index-page.client.tsx
+++ site/pages/index-page/src/index-page.client.tsx
@@ -3,16 +3,109 @@
 import type { IndexPageClientProps } from './index-page.interfaces.js'
 import type { FC }                   from 'react'
 
-import { useSuspenseQuery }          from '@apollo/client'
-
 import React                         from 'react'
+import { useRef }                    from 'react'
+import { useState }                  from 'react'
 
-import { GET_SITE_INDEX_PAGE_SEO }   from '@globals/data'
+import { Footer }                    from '@fragments/footer-fragment'
+import { Navigation }                from '@fragments/navigation-fragment'
+import { Articles }                  from '@site/articles-fragment'
+import { Hero }                      from '@site/hero-fragment'
+import { Services }                  from '@site/services-fragment'
+import { ServicesInfographics }      from '@site/services-infographics-fragment'
+import { WorksExamples }             from '@site/works-examples-fragment'
+import { Box }                       from '@ui/layout'
+import { Column }                    from '@ui/layout'
+import { getNavigationData }         from '@globals/data'
+import { getBlogPostsData }          from '@globals/data'
+import { getAvailableRadiiData }     from '@globals/data'
+import { getServicesData }           from '@globals/data'
+import { getFragmentsData }          from '@globals/data'
+import { getUiData }                 from '@globals/data'
+import { getWorkResultsData }        from '@globals/data'
+import { getCarBodiesData }          from '@globals/data'
+
+import { Seo }                       from './seo.component.js'
+import { useIndexPageClient }        from './hooks/use-index-page-client.hook.js'
 
 export const IndexPageClient: FC<IndexPageClientProps> = () => {
-  const { data } = useSuspenseQuery(GET_SITE_INDEX_PAGE_SEO)
-  console.log('DATA ON CLIENT:')
-  console.log(data)
+  const navigationData = getNavigationData()
+  const blogPostsData = getBlogPostsData()
+  const availableRadiiData = getAvailableRadiiData()
+  const servicesData = getServicesData()
+  const fragmentsData = getFragmentsData
+  const uiData = getUiData()
+  const workResultsData = getWorkResultsData()
+  const carBodiesData = getCarBodiesData()
 
-  return <>blaaaabbb</>
+  console.log('CLIENT DATA:')
+  console.log(navigationData)
+  console.log(blogPostsData)
+  console.log(availableRadiiData)
+  console.log(servicesData)
+  console.log(fragmentsData)
+  console.log(uiData)
+  console.log(workResultsData)
+  console.log(carBodiesData)
+
+  // const { SEO, ogCover, data } = props
+  // const headerRef = useRef<HTMLDivElement | null>(null)
+  // const isLoaded = useRef<boolean>(false)
+  //
+  // const [active, setActive] = useState<number>(0)
+  // const [scrollY, setScrollY] = useState<number>(0)
+  //
+  // const { getObserverOptions } = useIndexPageClient({ isLoaded, setActive, headerRef, setScrollY })
+  //
+  // return (
+  //   <Column ref={headerRef} width='100%' alignItems='center'>
+  //     <Box width='100%' justifyContent='center'>
+  //       <Column width='100%' alignItems='center'>
+  //         <Seo ogCover={ogCover} SEO={SEO} />
+  //         <Navigation
+  //           active={active}
+  //           navigationData={navigation}
+  //           availableRadiiData={availableRadii}
+  //           fragmentsData={fragments}
+  //           carBodiesData={carBodies}
+  //           servicesData={services}
+  //           scrollY={scrollY}
+  //         />
+  //         <Hero
+  //           fragmentsData={fragments}
+  //           uiData={ui}
+  //           contactsData={contacts}
+  //           availableRadiiData={availableRadii}
+  //           carBodiesData={carBodies}
+  //           servicesData={services}
+  //           navigationData={navigation}
+  //           {...getObserverOptions('hero')}
+  //         />
+  //       </Column>
+  //     </Box>
+  //     <Services
+  //       servicesData={services}
+  //       fragmentsData={fragments}
+  //       availableRadiiData={availableRadii}
+  //       {...getObserverOptions('services')}
+  //     />
+  //     <Articles
+  //       postsData={posts}
+  //       fragmentsData={fragments}
+  //       navigationData={navigation}
+  //       {...getObserverOptions('articles')}
+  //     />
+  //     <ServicesInfographics
+  //       uiData={ui}
+  //       fragmentsData={fragments}
+  //       {...getObserverOptions('infographics')}
+  //     />
+  //     <WorksExamples
+  //       workResultsData={workResults}
+  //       fragmentsData={fragments}
+  //       {...getObserverOptions('works-examples')}
+  //     />
+  //     <Footer fragmentsData={fragments} contactsData={contacts} />
+  //   </Column>
+  // )
 }
