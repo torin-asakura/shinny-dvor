56a038c1f013c4352038c79370fe965550d817c6
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-03 18:17:52 +0300

    style: format

--- fragments/navigation-fragment/src/navigation.component.tsx
+++ fragments/navigation-fragment/src/navigation.component.tsx
@@ -1,6 +1,6 @@
+import React                    from 'react'
 import { FC }                   from 'react'
 import { useState }             from 'react'
-import React                    from 'react'
 
 import { Booking }              from '@fragments/booking-fragment'
 import { INITIAL }              from '@store/booking'
