428e3212c491fb6552f86d9c26372179a6d0e76f
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-05 21:12:26 +0300

    fix(`@globals/data`): reexport `query types`

--- globals/data/src/queries/get-blog-posts-data/get-blog-posts-data.interface.ts
+++ globals/data/src/queries/get-blog-posts-data/get-blog-posts-data.interface.ts
@@ -2,3 +2,4 @@ import type { GetPostsQuery as BlogPostsQueryDataType } from '@globals/data'
 
 type PostsType = Exclude<BlogPostsQueryDataType['posts'], null | undefined>
 export type PostsDataType = PostsType['nodes']
+export type { BlogPostsQueryDataType }
--- globals/data/src/queries/get-services-data/get-services-data.interface.ts
+++ globals/data/src/queries/get-services-data/get-services-data.interface.ts
@@ -2,3 +2,4 @@ import type { GetServicesQuery as ServicesQueryDataType } from '@globals/data'
 
 type ServicesType = Exclude<ServicesQueryDataType['services'], null | undefined>
 export type ServicesDataType = ServicesType['nodes']
+export type { ServicesQueryDataType }
