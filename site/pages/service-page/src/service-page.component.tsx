d05063efd12e475b5a78f75489b7c9e35ff2175f
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-05 21:13:16 +0300

    feat(`@site/service-page`): wrap to `PreloadQuery`

--- site/pages/service-page/src/service-page.component.tsx
+++ site/pages/service-page/src/service-page.component.tsx
@@ -2,13 +2,29 @@ import type { ServicePageProps }       from './service-page.interfaces.js'
 
 import React                           from 'react'
 
+import { GET_SERVICE_BY }              from '@globals/data'
+import { PreloadQuery }                from '@globals/data'
+
 import { ServicePageClient }           from './service-page.client.js'
 import { runServicePageServerQueries } from './hooks/index.js'
 
 const ServicePage: ServicePageProps = async ({ params }) => {
   const serverQueryData = await runServicePageServerQueries({ params })
-  // @ts-expect-error param is not assignable
-  return <ServicePageClient params={params} serverQueryData={serverQueryData} />
+  const { uri } = params
+  return (
+    <PreloadQuery
+      query={GET_SERVICE_BY}
+      variables={{
+        uri,
+      }}
+    >
+      <ServicePageClient
+        // @ts-expect-error params is not assignable
+        params={params}
+        serverQueryData={serverQueryData}
+      />
+    </PreloadQuery>
+  )
 }
 
 export default ServicePage
