006d5a6c4eaa52ad563a3d1357e028656130173f
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-05 21:10:24 +0300

    feat(`@blog/post-page`): wrap to `PreloadQuery`

--- blog/pages/post-page/src/post-page.component.tsx
+++ blog/pages/post-page/src/post-page.component.tsx
@@ -2,11 +2,27 @@ import type { PostPageProps }        from './post-page.interfaces.js'
 
 import React                         from 'react'
 
+import { PreloadQuery }              from '@globals/data'
+import { GET_BLOG_POST }             from '@globals/data'
+
 import { PostPageClient }            from './post-page.client.js'
 import { runPostPageServerQuerires } from './hooks/index.js'
 
 export const PostPage: PostPageProps = async ({ params }) => {
   const serverQueryData = await runPostPageServerQuerires({ params })
-  // @ts-expect-error not assignable
-  return <PostPageClient params={params} serverQueryData={serverQueryData} />
+  const { uri } = params
+  return (
+    <PreloadQuery
+      query={GET_BLOG_POST}
+      variables={{
+        uri,
+      }}
+    >
+      <PostPageClient
+        // @ts-expect-error not assignable
+        params={params}
+        serverQueryData={serverQueryData}
+      />
+    </PreloadQuery>
+  )
 }
