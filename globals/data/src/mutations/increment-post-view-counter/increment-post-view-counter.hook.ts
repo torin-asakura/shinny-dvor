9956f5c82e2029e090083c1e2c55533df075b39e
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-06 18:45:36 +0300

    refactor(`@globals/data`): remove unused `console.log`

--- globals/data/src/mutations/increment-post-view-counter/increment-post-view-counter.hook.ts
+++ globals/data/src/mutations/increment-post-view-counter/increment-post-view-counter.hook.ts
@@ -5,8 +5,6 @@ import { POST_VIEW_COUNTER } from './increment-post-view-counter.mutation.js'
 const useIncrementPostViewCounter = () => {
   const [createIncrementPostViewCounterMutation] = useMutation(POST_VIEW_COUNTER)
 
-  console.log(createIncrementPostViewCounterMutation)
-
   const incrementViewCounter = async (postId: number): Promise<void> => {
     await createIncrementPostViewCounterMutation({
       variables: {
