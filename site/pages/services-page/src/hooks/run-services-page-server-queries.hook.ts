1ea486cc1205d5ac242c864bb94e02feea4b11d0
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-15 16:22:52 +0300

    feat(`site/services-page`): add generateMetadata hook

--- site/pages/services-page/src/hooks/run-services-page-server-queries.hook.ts
+++ site/pages/services-page/src/hooks/run-services-page-server-queries.hook.ts
@@ -1,7 +1,5 @@
 import type { ServicesPageServerProps } from '../services-page.interface.js'
 
-import { GET_SITE_SERVICES_PAGE_SEO }   from '@globals/data'
-import { GET_PREVIEW }                  from '@globals/data'
 import { GET_AVAILABLE_RADII }          from '@globals/data'
 import { GET_NAVIGATION }               from '@globals/data'
 import { GET_CONTACTS }                 from '@globals/data'
@@ -15,20 +13,6 @@ import { getServerClient }              from '@globals/data'
 export const runServicesPageServerQueries: ServicesPageServerProps = async () => {
   const client = getServerClient()
 
-  const { data: seoQueryData } = await client.query({ query: GET_SITE_SERVICES_PAGE_SEO })
-  const seoData = seoQueryData.pages.nodes[0].seo
-
-  const {
-    data: {
-      mediaItemBy: { sourceUrl: ogCover },
-    },
-  } = await client.query({
-    query: GET_PREVIEW,
-    variables: {
-      uri: '/cover/',
-    },
-  })
-
   await client.query({ query: GET_AVAILABLE_RADII })
   await client.query({ query: GET_NAVIGATION })
   await client.query({ query: GET_CONTACTS })
@@ -36,6 +20,4 @@ export const runServicesPageServerQueries: ServicesPageServerProps = async () =>
   await client.query({ query: GET_BLOG_POSTS })
   await client.query({ query: GET_CAR_BODIES })
   await client.query({ query: GET_SERVICES })
-
-  return { seoData, ogCover }
 }
