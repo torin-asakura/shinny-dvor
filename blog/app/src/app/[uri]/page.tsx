d218705e9620f8d2524fd57f0f4977420b4fae1f
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-15 16:18:43 +0300

    fix(`blog/app`): uncomment exports on index page

--- blog/app/src/app/[uri]/page.tsx
+++ blog/app/src/app/[uri]/page.tsx
@@ -1,11 +1,5 @@
-// export { revalidate } from '@blog/post-page'
-// export { dynamicParams } from '@blog/post-page'
-// export { generateStaticParams } from '@blog/post-page'
-// export { generateMetadata } from '@blog/post-page'
-// export { default } from '@blog/post-page'
-
-const Page = async () => {
-  return <>bla</>
-}
-
-export default Page
+export { revalidate } from '@blog/post-page'
+export { dynamicParams } from '@blog/post-page'
+export { generateStaticParams } from '@blog/post-page'
+export { generateMetadata } from '@blog/post-page'
+export { default } from '@blog/post-page'
