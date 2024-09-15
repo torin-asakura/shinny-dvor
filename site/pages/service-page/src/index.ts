eb96412dd01329a6f4c82c72d8310c5bdfc88df8
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-15 16:22:39 +0300

    feat(`site/service-page`): reexports `generateMetadata`

--- site/pages/service-page/src/index.ts
+++ site/pages/service-page/src/index.ts
@@ -1,3 +1,4 @@
 export { default } from './service-page.component.js'
+export { generateMetadata } from './hooks/generate-metadata.hook.js'
 export * from './service-page.constants.js'
 export * from './hooks/index.js'
