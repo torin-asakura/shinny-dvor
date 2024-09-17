cad32f95eafca741b479655de691eff63032686b
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-17 12:01:41 +0300

    feat(booking-telegram-bot): entrypoint init WIP

---
+++ booking-telegram-bot/entrypoints/service-entrypoint/src/bootstrap.ts
@@ -0,0 +1,28 @@
+import { NestLogger }                 from '@atls/nestjs-logger'
+import { NestFactory }                from '@nestjs/core'
+
+import { BotServiceEntrypointModule } from './bot-service-entrypoint.module.js'
+
+// declare const module: {
+//   hot: any
+//   accept: VoidFunction
+//   dispose: VoidFunction
+// }
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(BotServiceEntrypointModule, {
+    logger: new NestLogger(),
+  })
+
+  app.enableShutdownHooks()
+
+  await app.listen(3000)
+
+  // TODO for what?
+  // if (module.hot) {
+  //   module.hot.accept()
+  //   module.hot.dispose(() => app.close())
+  // }
+}
+
+bootstrap()
