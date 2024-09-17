3a0638cef26313dbd0617994cedd7bfc837162ea
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-17 16:50:06 +0300

    refactor(booking-telegram-bot/entrypoint): rm comments

--- booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
+++ booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
@@ -1,24 +1,7 @@
 import { TelegramAdapterModule } from '@booking-telegram-bot/telegram-adapter-module'
 import { Module }                from '@nestjs/common'
 
-// import { BotApplicationModule }    from '@bot/application-module'
-// import { CqrsAdapterModule }       from '@bot/cqrs-adapter-module'
-// import { BotInfrastructureModule } from '@bot/infrastructure-module'
-// import { S3AdapterModule }         from '@bot/s3-adapter-module'
-// import { StripeAdapterModule }     from '@bot/stripe-adapter'
-// import { TelegramAdapterModule }   from '@bot/telegram-adapter-module'
-// import { TypeormAdapterModule }    from '@bot/typeorm-adapter-module'
-
 @Module({
-  imports: [
-    TelegramAdapterModule.register(),
-    //     CqrsAdapterModule.register(),
-    //     TelegramAdapterModule.register(),
-    //     TypeormAdapterModule.register(),
-    //     S3AdapterModule.register(),
-    //     BotApplicationModule.register(),
-    //     BotInfrastructureModule.register(),
-    //     StripeAdapterModule.register(),
-  ],
+  imports: [TelegramAdapterModule.register()],
 })
 export class BotServiceEntrypointModule {}
