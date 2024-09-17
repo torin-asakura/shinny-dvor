9c43342e51728a362233e73a0d292f45a3354183
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-17 16:48:34 +0300

    feat(booking-telegram-bot): bot works in module WIP

--- booking-telegram-bot/adapters/telegram-adapter/src/module/telegram-adapter.module.ts
+++ booking-telegram-bot/adapters/telegram-adapter/src/module/telegram-adapter.module.ts
@@ -1,19 +1,25 @@
-import { Module } from '@nestjs/common'
+import { Module }         from '@nestjs/common'
+
+import { Snake }          from 'tgsnake'
+
+import { TGSHAKE_CONFIG } from '../config/index.js'
+import { TestService }    from '../services/index.js'
+
+const client = new Snake(TGSHAKE_CONFIG)
 
 @Module({})
 export class TelegramAdapterModule {
   static register() {
-    console.log('register module')
+    client.on('msg.text', (update) => {
+      return update.msg?.reply('I hear You!')
+    })
+
+    client.run()
 
     return {
       global: true,
+      providers: [TestService],
       module: TelegramAdapterModule,
     }
-
-    // client.on('msg.text', (update) => {
-    //   return update.msg?.reply('I hear You!')
-    // })
-    //
-    // client.run()
   }
 }
