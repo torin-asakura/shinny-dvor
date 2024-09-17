0ff3bb885b9a0715b94d26c614edf98f2a3197b0
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-17 12:01:19 +0300

    feat(booking-telegram-bot): add tgsnake.config WIP

---
+++ booking-telegram-bot/adapters/telegram-adapter/tgsnake.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tgsnake').Options} */
+const config = {
+  apiHash: '1456d21e179cc0d3165bd3f65424db87',
+  apiId: 27275852,
+  logLevel: 'error',
+  plugins: [],
+  clientOptions: {},
+  login: {
+    sessionName: 'myapp',
+    forceDotSession: true,
+    botToken: '6451409399:AAENpt4tPx8m55kl-1QEQyEnAnvY4gaJ7rc',
+  },
+  experimental: {},
+}
+export default config
