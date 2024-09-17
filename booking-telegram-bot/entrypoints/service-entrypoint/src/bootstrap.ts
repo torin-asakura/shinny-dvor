baeb5252142bde618ff118451a0c144bee8cab56
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-17 16:49:51 +0300

    fix(booking-telegram-bot/entrypoint): add hmr

--- booking-telegram-bot/entrypoints/service-entrypoint/src/bootstrap.ts
+++ booking-telegram-bot/entrypoints/service-entrypoint/src/bootstrap.ts
@@ -3,11 +3,11 @@ import { NestFactory }                from '@nestjs/core'
 
 import { BotServiceEntrypointModule } from './bot-service-entrypoint.module.js'
 
-// declare const module: {
-//   hot: any
-//   accept: VoidFunction
-//   dispose: VoidFunction
-// }
+declare const module: {
+  hot: any
+  accept: VoidFunction
+  dispose: VoidFunction
+}
 
 const bootstrap = async () => {
   const app = await NestFactory.create(BotServiceEntrypointModule, {
@@ -18,11 +18,10 @@ const bootstrap = async () => {
 
   await app.listen(3000)
 
-  // TODO for what?
-  // if (module.hot) {
-  //   module.hot.accept()
-  //   module.hot.dispose(() => app.close())
-  // }
+  if (module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => app.close())
+  }
 }
 
 bootstrap()
