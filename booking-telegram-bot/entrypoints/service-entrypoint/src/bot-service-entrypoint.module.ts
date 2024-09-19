0f8ebcf702d2f3b5dd8f3e0416c5b7ecdb323435
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-19 19:46:11 +0300

    refactor(booking-telegram-bot): register application module on service entrypoint

--- booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
+++ booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
@@ -1,11 +1,13 @@
 import { Module }                          from '@nestjs/common'
-import { TelegramBotInfrastructureModule } from '@telegram-bot/infrastructure-module'
 
 import { TgsnakeAdapterModule }            from '@booking-telegram-bot/tgsnake-adapter-module'
+import { TelegramBotApplicationModule }    from '@telegram-bot/application-module'
+import { TelegramBotInfrastructureModule } from '@telegram-bot/infrastructure-module'
 
 @Module({
   imports: [
     TgsnakeAdapterModule.registerAsync(),
+    TelegramBotApplicationModule.register(),
     TelegramBotInfrastructureModule.registerAsync(),
   ],
 })
