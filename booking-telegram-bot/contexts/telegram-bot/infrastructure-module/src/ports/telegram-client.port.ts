efebaf2a95837375d3b240801d5efe0a226067ef
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-19 19:45:16 +0300

    refactor(booking-telegram-bot): provide listener to port

--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/ports/telegram-client.port.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/ports/telegram-client.port.ts
@@ -1,24 +1,16 @@
 import { Injectable }            from '@nestjs/common'
 import { Logger }                from '@nestjs/common'
-import { TelegramClientPort }    from '@telegram-bot/application-module'
 
 import { TgsnakeAdapterService } from '@booking-telegram-bot/tgsnake-adapter-module'
+import { TelegramClientPort }    from '@telegram-bot/application-module'
 
 @Injectable()
 export class TelegramClientPortImpl implements TelegramClientPort {
-  private readonly logger = new Logger('TelegramClientPort')
-
   // TODO почему на этом уровне конструктор завязан на tgsnake?
   // TODO почему зависимости добавленны и в дев и в пир?
   constructor(private readonly telegramClient: TgsnakeAdapterService) {}
 
-  async listenMessage() {
-    this.logger.debug('log on video converter port')
-
-    this.telegramClient.on('msg.text', (update) => {
-      return update.msg?.reply('I hear You!')
-    })
-
-    return
+  async listen() {
+    return this.telegramClient.listenMessage()
   }
 }
