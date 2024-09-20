1d6e167a59a73856b742bc8d17413d4ffebb7975
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-20 11:58:52 +0300

    feat(booking-telegram-bot): converstaion init

--- booking-telegram-bot/adapters/tgsnake-adapter/src/services/tgsnake-adapter.service.ts
+++ booking-telegram-bot/adapters/tgsnake-adapter/src/services/tgsnake-adapter.service.ts
@@ -1,5 +1,4 @@
 import { Injectable }     from '@nestjs/common'
-
 import { Snake }          from 'tgsnake'
 
 import { TGSHAKE_CONFIG } from '../config/index.js'
@@ -17,19 +16,49 @@ export class TgsnakeAdapterService extends Snake {
   async listenMessage() {
     // TODO interfaces
     const reply = async (ctx, text) => {
-      return ctx.message.reply(text)
+      return await ctx.message.reply(text)
+    }
+
+    const runConversationA1 = async (ctx) => {
+      await reply(ctx, 'start conversation')
+      const conversation = this.conversation.create(ctx.message.chat.id)
+      await reply(ctx, 'input a')
+
+      // await reply(ctx, 'input kuzov (need to query. look at site form)')
+      // await reply(ctx, 'answer with keyboard button')
+      // await reply(ctx, 'with cancel button')
+      // await reply(ctx, 'check correct answer')
+
+      const response1 = await conversation.wait('msg.text')
+      await reply(response1, 'input b')
+
+      const response2 = await conversation.wait('msg.text', (update) => {
+        if (update.message.text.toLowerCase() === 'b') {
+          return true
+        }
+        update.message.reply('Input B')
+        return false
+      })
+
+      response2.message.reply('Done')
+
+      this.conversation.remove(ctx.message.chat.id)
     }
 
     this.cmd('start', async (ctx) => {
-      reply(ctx, 'command start')
+      // TODO remove any conversation, than:
+      await reply(ctx, 'welcome message')
+      await runConversationA1(ctx)
     })
 
     this.cmd('help', async (ctx) => {
-      reply(ctx, 'command help')
+      await reply(ctx, 'command help')
     })
 
-    this.on('msg.text', (ctx) => {
-      return reply(ctx, 'message received')
+    this.on('msg.text', async (ctx) => {
+      if (!this.conversation.conversation.size) {
+        await runConversationA1(ctx)
+      }
     })
   }
 }
