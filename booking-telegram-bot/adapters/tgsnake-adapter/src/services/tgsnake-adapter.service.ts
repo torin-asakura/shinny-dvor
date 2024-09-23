68194d9419e108eb4740de04f79dd7aef90b86a0
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-23 19:27:39 +0300

    refactor(booking-telegram-bot/adapter): separate all login to `useCases` and `services` in application layer; create some base methods

--- booking-telegram-bot/adapters/tgsnake-adapter/src/services/tgsnake-adapter.service.ts
+++ booking-telegram-bot/adapters/tgsnake-adapter/src/services/tgsnake-adapter.service.ts
@@ -12,116 +12,60 @@ import { TGSHAKE_CONFIG } from '../config/index.js'
 
 @Injectable()
 export class TgsnakeAdapterService extends Snake {
+  // TODO types
   constructor() {
     super(TGSHAKE_CONFIG)
     this.run()
   }
 
-  async listenMessage() {
+  private getRandomBigInt() {
+    const randomHex = crypto.randomBytes(4).toString('hex')
+    const randomBigInt = BigInt(parseInt(randomHex, 16))
+    return randomBigInt
+  }
+
+  // TODO types
+  async reply(ctx: any, text: string) {
+    return await ctx.message.reply(text)
+  }
+
+  async sendMessage(ctx: any, text: string) {
+    const { from: userMessage } = ctx.message
+
+    const { id: userId } = userMessage
+    const { accessHash } = userMessage
+
+    const randomBigInt = this.getRandomBigInt()
+
+    return await ctx.api.invoke(
+      new Raw.messages.SendMessage({
+        message: text,
+        peer: new Raw.InputPeerUser({ userId, accessHash }),
+        randomId: randomBigInt,
+      })
+    )
+  }
+
+  // TODO interface
+  async createConversation(ctx: any) {
+    return this.conversation.create(ctx.message.chat.id)
+  }
+
+  // TODO interface
+  async removeConversation(ctx: any) {
+    this.conversation.remove(ctx.message.chat.id)
+  }
+
+  async listen() {
     // TODO interfaces
-    const reply = async (ctx, text) => {
-      return await ctx.message.reply(text)
-    }
-
-    const sendMessage = async (ctx, text) => {
-      const { from: userMessage } = ctx.message
-
-      const { id: userId } = userMessage
-      const { accessHash } = userMessage
-
-      // TODO to globals
-      const randomHex = crypto.randomBytes(4).toString('hex')
-      const randomBigInt = BigInt(parseInt(randomHex, 16))
-
-      ctx.api.invoke(
-        new Raw.messages.SendMessage({
-          message: text,
-          peer: new Raw.InputPeerUser({ userId, accessHash }),
-          randomId: randomBigInt,
-        })
-      )
-    }
-
-    const removeConversation = async (ctx) => {
-      this.conversation.remove(ctx.message.chat.id)
-    }
-
-    const runConversationA1 = async (ctx) => {
-      const conversation = this.conversation.create(ctx.message.chat.id)
-      await sendMessage(ctx, 'start conversation')
-
-      // TODO Q: name? - save it from context
-      // TODO Q: phone? - есть кнопка в telegram api - типо поделиться контактом - сделать опциональной
-
-      // TODO keyboard with cancel button
-      await sendMessage(ctx, 'kuzov auto quesiton*')
-
-      // TODO R: check response
-      const response1 = await conversation.wait('msg.text', ({ message }) => {
-        if (message.text.toLowerCase() === 'b') {
-          return true
-        }
-        message.reply('Input B')
-        return false
-      })
-
-      // TODO keyboard with cancel button
-      await sendMessage(ctx, 'diameter coles*')
-
-      // TODO R: check response
-      const response2 = await conversation.wait('msg.text', (update) => {
-        if (update.message.text.toLowerCase() === 'b') {
-          return true
-        }
-        update.message.reply('Input B')
-        return false
-      })
-
-      // TODO keyboard with cancel button
-      await sendMessage(ctx, 'tip remonta*')
-
-      // TODO R: check response
-      const response3 = await conversation.wait('msg.text', (update) => {
-        if (update.message.text.toLowerCase() === 'b') {
-          return true
-        }
-        update.message.reply('Input B')
-        return false
-      })
-
-      // TODO keyboard with cancel and skip button
-      await sendMessage(ctx, 'commentary*')
-
-      // TODO R: check response
-      const response4 = await conversation.wait('msg.text', (update) => {
-        if (update.message.text.toLowerCase() === 'b') {
-          return true
-        }
-        update.message.reply('Input B')
-        return false
-      })
-
-      await sendMessage(ctx, 'done')
-      await sendMessage(ctx, 'предложение отменить или перенести запись')
-      await sendMessage(ctx, 'ссылки на оператора (другой чат)')
-
-      removeConversation(ctx)
-    }
-
-    this.cmd('start', async (ctx) => {
-      removeConversation(ctx)
-      await reply(ctx, 'welcome message')
-      await runConversationA1(ctx)
-    })
-
-    this.cmd('help', async (ctx) => {
-      await reply(ctx, 'command help')
-    })
-
-    this.on('msg.text', async (ctx) => {
-      if (!this.conversation?.conversation?.size) {
-        await runConversationA1(ctx)
-      }
-    })
+    //   const removeConversation = async (ctx) => {
+    //     this.conversation.remove(ctx.message.chat.id)
+    //   }
+    //
+    //   this.on('msg.text', async (ctx) => {
+    //     if (!this.conversation?.conversation?.size) {
+    //       await runConversationA1(ctx)
+    //     }
+    //   })
   }
 }
