8ea4794c14738b71367fc8f63b1695723b3171a6
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-23 19:28:47 +0300

    feat(booking-telegram-bot/application): add services to module

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/module/telegram-bot.application.module.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/module/telegram-bot.application.module.ts
@@ -1,15 +1,17 @@
+import * as useCases          from '../use-cases/index.js'
+import * as services          from '../services/index.js'
+
 import type { DynamicModule } from '@nestjs/common'
 
 import { Module }             from '@nestjs/common'
-
-import * as useCases          from '../use-cases/index.js'
 
 @Module({})
 export class TelegramBotApplicationModule {
   static register(): DynamicModule {
     const useCaseProviders = Object.values(useCases)
+    const serviceProviders = Object.values(services)
 
-    const providers = [...useCaseProviders]
+    const providers = [...useCaseProviders, ...serviceProviders]
     const exports = [...useCaseProviders]
 
     return {
