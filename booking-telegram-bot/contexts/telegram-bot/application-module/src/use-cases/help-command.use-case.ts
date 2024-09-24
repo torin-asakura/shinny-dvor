93ace37d81f87824fdb4f6cf3bd6eafa71f3659e
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-24 20:42:00 +0300

    fix(bot/infrastructure): remove unused

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/use-cases/help-command.use-case.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/use-cases/help-command.use-case.ts
@@ -8,9 +8,6 @@ export class HelpCommandUseCase {
 
   // TODO interface
   async execute(ctx: any) {
-    // removeConversation(ctx)
-    // await reply(ctx, 'welcome message')
-    // await runConversationA1(ctx)
     await this.telegramClient.sendMessage(ctx, 'help message')
   }
 }
