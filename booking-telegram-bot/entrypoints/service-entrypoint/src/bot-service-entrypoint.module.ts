ed8309277fecfdf65d898fa0844a5eb32c106708
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-24 20:39:42 +0300

    feat(bot-service-entrypoint): add graphql modules

--- booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
+++ booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
@@ -1,14 +1,20 @@
-import { Module }                          from '@nestjs/common'
+import { ApolloAdapterModule }               from '@booking-telegram-bot/apollo-adapter'
+import { TgsnakeAdapterModule }              from '@booking-telegram-bot/tgsnake-adapter'
+import { GraphqlClientApplicationModule }    from '@graphql-client/application-module'
+import { GraphqlClientInfrastructureModule } from '@graphql-client/infrastructure-module'
+import { Module }                            from '@nestjs/common'
 
-import { TgsnakeAdapterModule }            from '@booking-telegram-bot/tgsnake-adapter-module'
-import { TelegramBotApplicationModule }    from '@telegram-bot/application-module'
-import { TelegramBotInfrastructureModule } from '@telegram-bot/infrastructure-module'
+import { TelegramBotApplicationModule }      from '@telegram-bot/application-module'
+import { TelegramBotInfrastructureModule }   from '@telegram-bot/infrastructure-module'
 
 @Module({
   imports: [
-    TgsnakeAdapterModule.registerAsync(),
+    TgsnakeAdapterModule.register(),
     TelegramBotApplicationModule.register(),
     TelegramBotInfrastructureModule.registerAsync(),
+    ApolloAdapterModule.register(),
+    GraphqlClientInfrastructureModule.register(),
+    GraphqlClientApplicationModule.register(),
   ],
 })
 export class BotServiceEntrypointModule {}
