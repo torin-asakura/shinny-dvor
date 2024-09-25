6fb51946c6529d57bbe911907ed10db5af87ce95
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-25 16:44:06 +0300

    feat(bot/application): ask and filter answers

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/services/conversation.service.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/conversation.service.ts
@@ -1,12 +1,14 @@
-import { Injectable }          from '@nestjs/common'
+import { Injectable }                              from '@nestjs/common'
 
-import { GET_CAR_BODIES }      from '@globals/data'
-import { GET_AVAILABLE_RADII } from '@globals/data'
-import { GET_SERVICES }        from '@globals/data'
-import { RunQueryUseCase }     from '@graphql-client/application-module'
-import { checkArrayLength }    from '@globals/data'
+import { GET_CAR_BODIES }                          from '@globals/data'
+import { GET_AVAILABLE_RADII }                     from '@globals/data'
+import { GET_SERVICES }                            from '@globals/data'
+import { RunQueryUseCase }                         from '@graphql-client/application-module'
+import { checkArrayLength }                        from '@globals/data'
 
-import { TelegramClientPort }  from '../ports/index.js'
+import { TelegramClientPort }                      from '../ports/index.js'
+import { CANCEL_APPOINTMENT_BUTTON_TEXT }          from './conversation.constants.js'
+import { CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT } from './conversation.constants.js'
 
 @Injectable()
 export class ConversationService {
@@ -62,10 +64,7 @@ export class ConversationService {
   // TODO inteface
   async process(ctx: any) {
     try {
-      // TODO есть функции, которые мы используем, напрмер в `@site`
-      // для того, чтобы вытащить необходимые данные их приходящей `queryData`
-      // используй эти функции здесь тоже
-      // может быть их нужно будет переместить в `@globals`
+      const conversationData: Record<string, string> = {}
 
       const carBodiesData = await this.getCarBodiesData()
       const radiiData = await this.getRadiiData()
@@ -74,68 +73,114 @@ export class ConversationService {
       const carBodyTitles = this.getCarBodyTitles(carBodiesData)
       const radiiTitles = this.getRadiiTitles(radiiData)
       const serviceTitles = this.getServiceTitles(servicesData)
-      console.log(radiiTitles)
-      console.log(serviceTitles)
 
       await this.telegramClient.sendMessage(ctx, 'start conversation')
 
       // TODO create createConversation method on adapter
       const conversation = await this.telegramClient.createConversation(ctx)
-      console.log(conversation)
+      // console.log(conversation)
+
+      // TODO keyboard with cancel button
+      await this.telegramClient.sendMessageWithMarkup(ctx, 'kuzov auto quesiton*', [
+        ...carBodyTitles,
+        CANCEL_APPOINTMENT_BUTTON_TEXT,
+      ])
+
+      // TODO check interfaces
+      await conversation.wait('msg.text', ({ message }) => {
+        const { text: responseText } = message
+
+        // TODO switch case
+        if (responseText === CANCEL_APPOINTMENT_BUTTON_TEXT || responseText === '/cancel') {
+          console.log('cancel conversation')
+        } else if (carBodyTitles.includes(responseText)) {
+          conversationData.carBodyTitle = responseText
+          return true
+        }
+
+        message.reply('Выберите ответ на клавиатуре, либо нажмите /cancel, чтобы отменить запись')
+        return false
+      })
+
+      // TODO keyboard with cancel button
+      await this.telegramClient.sendMessageWithMarkup(ctx, 'diameter coles*', [
+        ...radiiTitles,
+        CANCEL_APPOINTMENT_BUTTON_TEXT,
+      ])
+
+      await conversation.wait('msg.text', ({ message }) => {
+        const { text: responseText } = message
+
+        // TODO switch case
+        if (responseText === CANCEL_APPOINTMENT_BUTTON_TEXT || responseText === '/cancel') {
+          console.log('cancel conversation')
+        } else if (radiiTitles.includes(responseText)) {
+          conversationData.radiiTitle = responseText
+          return true
+        }
+
+        message.reply('Выберите ответ на клавиатуре, либо нажмите /cancel, чтобы отменить запись')
+        return false
+      })
+
+      await this.telegramClient.sendMessageWithMarkup(ctx, 'tip remonta*', [
+        ...serviceTitles,
+        CANCEL_APPOINTMENT_BUTTON_TEXT,
+      ])
+
+      await conversation.wait('msg.text', ({ message }) => {
+        const { text: responseText } = message
+
+        // TODO switch case
+        if (responseText === CANCEL_APPOINTMENT_BUTTON_TEXT || responseText === '/cancel') {
+          console.log('cancel conversation')
+        } else if (serviceTitles.includes(responseText)) {
+          conversationData.serviceTitle = responseText
+          return true
+        }
+
+        message.reply('Выберите ответ на клавиатуре, либо нажмите /cancel, чтобы отменить запись')
+        return false
+      })
+
+      await this.telegramClient.sendMessageWithMarkup(ctx, 'commentary*', [
+        CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT,
+        CANCEL_APPOINTMENT_BUTTON_TEXT,
+      ])
+
+      await conversation.wait('msg.text', ({ message }) => {
+        const { text: responseText } = message
+
+        // TODO switch case
+        if (responseText === CANCEL_APPOINTMENT_BUTTON_TEXT || responseText === '/cancel') {
+          console.log('cancel conversation')
+        } else if (responseText === CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT) {
+          console.log('continue without commentary')
+          return true
+        }
+
+        conversationData.commentary = responseText
+        return true
+      })
+
+      const { carBodyTitle, radiiTitle, serviceTitle, commentary } = conversationData
+
+      let approvalMessage = ''
+      approvalMessage += `Тип кузова: ${carBodyTitle}\n`
+      approvalMessage += `Диаметр колёс: ${radiiTitle}\n`
+      approvalMessage += `Тип ремонта: ${serviceTitle}\n`
+      if (commentary) approvalMessage += `Комментарий: ${commentary}`
+
+      await this.telegramClient.sendMessage(ctx, approvalMessage)
 
       // TODO Q: name? - save it from context
       // TODO Q: phone? - есть кнопка в telegram api - типо поделиться контактом - сделать опциональной
 
-      // TODO keyboard with cancel button
-      await this.telegramClient.sendMessage(ctx, 'kuzov auto quesiton*')
-
-      // TODO R: check response
-      const response1 = await conversation.wait('msg.text', ({ message }) => {
-        if (message.text.toLowerCase() === 'b') {
-          return true
-        }
-        message.reply('Input B')
-        return false
-      })
-
-      // TODO keyboard with cancel button
-      await this.telegramClient.sendMessage(ctx, 'diameter coles*')
-
-      // TODO R: check response
-      const response2 = await conversation.wait('msg.text', (update) => {
-        if (update.message.text.toLowerCase() === 'b') {
-          return true
-        }
-        update.message.reply('Input B')
-        return false
-      })
-
-      // TODO keyboard with cancel button
-      await this.telegramClient.sendMessage(ctx, 'tip remonta*')
-
-      // TODO R: check response
-      const response3 = await conversation.wait('msg.text', (update) => {
-        if (update.message.text.toLowerCase() === 'b') {
-          return true
-        }
-        update.message.reply('Input B')
-        return false
-      })
-
-      // TODO keyboard with cancel and skip button
-      await this.telegramClient.sendMessage(ctx, 'commentary*')
-
-      // TODO R: check response
-      const response4 = await conversation.wait('msg.text', (update) => {
-        if (update.message.text.toLowerCase() === 'b') {
-          return true
-        }
-        update.message.reply('Input B')
-        return false
-      })
-
-      await this.telegramClient.sendMessage(ctx, 'done')
-      await this.telegramClient.sendMessage(ctx, 'предложение отменить или перенести запись')
+      // TODO approve with keyboard
+      await this.telegramClient.sendMessage(
+        ctx,
+        'предложение согласовать отменить или перенести запись'
+      )
       await this.telegramClient.sendMessage(ctx, 'ссылки на оператора (другой чат)')
 
       this.telegramClient.removeConversation(ctx)
