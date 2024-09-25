b0a6631074cb8b5ab042f21612159922d55e69f1
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-25 18:41:32 +0300

    refactor(bot/telegram-bot/application): add date constants

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/services/conversation.constants.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/conversation.constants.ts
@@ -1,3 +1,12 @@
 // TODO to messages (locales)
 export const CANCEL_APPOINTMENT_BUTTON_TEXT = 'Отменить запись'
 export const CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT = 'Продолжить без комментария'
+export const APPROVE_APPOINTMENT_BUTTON_TEXT = 'Согласовать запись'
+export const EDIT_APPOINTMENT_BUTTON_TEXT = 'Изменить запись'
+
+export const DAY_MILLISECONDS = 1000 * 60 * 60 * 24
+export const DATE_OPTIONS = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long',
+}
