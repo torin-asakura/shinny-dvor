aee4491c91f6641101af1fb36f54f99ddf7e0d7b
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-27 20:40:05 +0300

    feat(bot/application-module): get-commentary-conversation-part init

---
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment.get-commentary.conversation-part.ts
@@ -0,0 +1,42 @@
+import { Injectable }                              from '@nestjs/common'
+
+import { GET_SERVICES }                            from '@globals/data'
+import { RunQueryUseCase }                         from '@graphql-client/application-module'
+import { checkArrayLength }                        from '@globals/data'
+
+import { TelegramClientPort }                      from '../ports/index.js'
+import { CANCEL_APPOINTMENT_BUTTON_TEXT }          from './appointment.constants.js'
+import { CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT } from './appointment.constants.js'
+
+// TODO create conversationPart Class with createConversation method and extend that class
+
+@Injectable()
+export class AppointmentGetCommentaryConversationPart {
+  // TODO interfaces
+  servicesData: any
+  serviceTitles: Array<string>
+
+  constructor(private readonly telegramClient: TelegramClientPort) {}
+
+  async sendQuestion(ctx) {
+    await this.telegramClient.sendMessageWithMarkup(ctx, 'commentary*', [
+      CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT,
+      CANCEL_APPOINTMENT_BUTTON_TEXT,
+    ])
+  }
+
+  checkAnswer(ctx) {
+    const { message } = ctx
+    const { text: responseText } = message
+
+    // TODO switch case
+    if (responseText === CANCEL_APPOINTMENT_BUTTON_TEXT || responseText === '/cancel') {
+      console.log('cancel appointment')
+    } else if (responseText === CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT) {
+      console.log('continue without commentary')
+      return true
+    }
+
+    return ' '
+  }
+}
