a6e54da4c18858f46e95f87afa5c121f51109334
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-27 20:38:39 +0300

    refactor(bot/application-module): mv services constants

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment.constants.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment.constants.ts
@@ -3,9 +3,11 @@ export const CANCEL_APPOINTMENT_BUTTON_TEXT = 'Отменить запись'
 export const CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT = 'Продолжить без комментария'
 export const APPROVE_APPOINTMENT_BUTTON_TEXT = 'Согласовать запись'
 export const EDIT_APPOINTMENT_BUTTON_TEXT = 'Изменить запись'
+export const TIME_SLOT_STEP_MIN = 30
 
-export const DAY_MILLISECONDS = 1000 * 60 * 60 * 24
-export const DATE_OPTIONS = {
+export const DAY_MS = 1000 * 60 * 60 * 24
+export const SUGGESTED_DAYS_QUANTITY = 7
+export const DATE_OPTIONS: Intl.DateTimeFormatOptions = {
   weekday: 'long',
   day: 'numeric',
   month: 'long',
