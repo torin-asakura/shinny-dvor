505611d0daf52b7954e172c9daf9090eed98a236
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-27 20:39:51 +0300

    feat(bot/application-module): car-body-conversation-part init

---
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment.get-car-body.conversation-part.ts
@@ -0,0 +1,68 @@
+import { Injectable }                     from '@nestjs/common'
+
+import { GET_CAR_BODIES }                 from '@globals/data'
+import { RunQueryUseCase }                from '@graphql-client/application-module'
+import { checkArrayLength }               from '@globals/data'
+
+import { TelegramClientPort }             from '../ports/index.js'
+import { CANCEL_APPOINTMENT_BUTTON_TEXT } from './appointment.constants.js'
+
+// TODO create conversationPart Class with createConversation method and extend that class
+
+@Injectable()
+export class AppointmentGetCarBodyConversationPart {
+  // TODO interfaces
+  carBodiesData: any
+  carBodyTitles: Array<string>
+
+  constructor(
+    private readonly telegramClient: TelegramClientPort,
+    private readonly runQueryUseCase: RunQueryUseCase
+  ) {}
+
+  private async getCarBodiesData() {
+    // TODO income interfaces
+    const queryData = await this.runQueryUseCase.execute(GET_CAR_BODIES)
+    const carBodiesQueryData = queryData.data.carBodyItems.nodes
+
+    checkArrayLength({ carBodiesQueryData })
+
+    return carBodiesQueryData
+  }
+
+  // TODO interfaces
+  private getCarBodyTitles() {
+    return this.carBodiesData.map((singleCarData: any) => singleCarData.contentAddons.title)
+  }
+
+  private async initData() {
+    this.carBodiesData = this.getCarBodiesData()
+    this.carBodyTitles = this.getCarBodyTitles()
+  }
+
+  async sendQuestion(ctx) {
+    await this.initData()
+
+    // TODO keyboard with cancel button
+    await this.telegramClient.sendMessageWithMarkup(ctx, 'kuzov auto quesiton*', [
+      ...this.carBodyTitles,
+      CANCEL_APPOINTMENT_BUTTON_TEXT,
+    ])
+  }
+
+  checkAnswer(ctx) {
+    const { message } = ctx
+
+    const { text: responseText } = message
+
+    // TODO switch case
+    if (responseText === CANCEL_APPOINTMENT_BUTTON_TEXT || responseText === '/cancel') {
+      console.log('cancel appointment')
+    } else if (this.carBodyTitles.includes(responseText)) {
+      return responseText
+    }
+
+    message.reply('Выберите ответ на клавиатуре, либо нажмите /cancel, чтобы отменить запись')
+    return false
+  }
+}
