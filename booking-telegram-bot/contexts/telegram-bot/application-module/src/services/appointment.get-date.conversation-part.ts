dc679f86f459aab8cdc9830e6aac184c2df7ff11
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-27 20:40:28 +0300

    feat(bot/application-module): get-date-conversation-part init

---
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment.get-date.conversation-part.ts
@@ -0,0 +1,82 @@
+import { Injectable }                     from '@nestjs/common'
+
+import { TelegramClientPort }             from '../ports/index.js'
+import { DATE_OPTIONS }                   from './appointment.constants.js'
+import { DAY_MS }                         from './appointment.constants.js'
+import { SUGGESTED_DAYS_QUANTITY }        from './appointment.constants.js'
+import { CANCEL_APPOINTMENT_BUTTON_TEXT } from './appointment.constants.js'
+
+// TODO create conversationPart Class with createConversation method
+
+@Injectable()
+export class AppointmentGetDateConversationPart {
+  // TODO types
+  suggestedDates: any
+  keyboardVariants: any
+
+  constructor(private readonly telegramClient: TelegramClientPort) {
+    this.suggestedDates = this.getSuggestedDates()
+    this.keyboardVariants = this.getKeyboardVariants()
+  }
+
+  private getSuggestedDates() {
+    const suggestedDates = Array.apply(null, Array(SUGGESTED_DAYS_QUANTITY)).map((_, i) => {
+      const daysFromNowMs = DAY_MS * i
+      const milliseconds = Date.now() + daysFromNowMs
+      const suggestedDate = new Date(milliseconds)
+      suggestedDate.setHours(0, 0, 0, 0)
+
+      const suggestedDateMs = suggestedDate.getTime()
+
+      let clientText = suggestedDate.toLocaleDateString('ru-RU', DATE_OPTIONS)
+      if (!i) clientText += ' (сегодня)'
+
+      return {
+        clientText,
+        milliseconds: suggestedDateMs,
+      }
+    })
+
+    return suggestedDates
+  }
+
+  private getKeyboardVariants() {
+    return this.suggestedDates.map(({ clientText }) => clientText)
+  }
+
+  async sendQuestion(ctx) {
+    // TODO to locales
+    const MESSAGE_TEXT = 'Выберите дату записи'
+
+    await this.telegramClient.sendMessageWithMarkup(ctx, MESSAGE_TEXT, [
+      ...this.keyboardVariants,
+      CANCEL_APPOINTMENT_BUTTON_TEXT,
+    ])
+  }
+
+  // TODO to constructor
+  private checkCancelCondition(text: string) {
+    return text === CANCEL_APPOINTMENT_BUTTON_TEXT || text === '/cancel'
+  }
+
+  checkAnswer(ctx) {
+    const { message } = ctx
+    // TODO нужно выносить метод wait в адаптер?
+    // 	он относится к библиотеке, а вызываю я его тут
+    const { text: responseText } = message
+
+    // TODO switch case
+    // TODO move it into top layer. cancel current operation
+
+    if (this.checkCancelCondition(responseText)) {
+      message.reply('Запись отменена')
+      return this.telegramClient.removeConversation(ctx)
+    } else if (this.keyboardVariants.includes(responseText)) {
+      const date = this.suggestedDates.find(({ clientText }) => clientText === responseText)
+      return date
+    }
+
+    message.reply('Выберите ответ на клавиатуре, либо нажмите /cancel, чтобы отменить запись')
+    return false
+  }
+}
