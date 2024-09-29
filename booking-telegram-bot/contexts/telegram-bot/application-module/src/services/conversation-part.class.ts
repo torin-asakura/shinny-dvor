f339bc87974e384ce4250ba70c02e447e4a8e003
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-29 19:57:51 +0300

    feat(bot/application): add `conversation-part` parent class

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/services/conversation-part.class.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/conversation-part.class.ts
@@ -1,1 +1,31 @@
+class ConversationPart {
+  conversationPartName: string
 
+  async sendQuestion() {}
+  async checkAnswer() {}
+
+  checkAnswerCondition(checkAnswerResult) {
+    return checkAnswerResult
+  }
+
+  async process(ctx, conversation, processData) {
+    const questionData = processData?.questionData
+
+    await this.sendQuestion(ctx, questionData)
+
+    await conversation.wait('msg.text', (ctx) => {
+      const checkAnswerResult = this.checkAnswer(ctx)
+
+      // TODO change namings
+      // TODO это условия записи в память диалога
+      if (this.checkAnswerCondition(checkAnswerResult)) {
+        conversation.data[this.conversationPartName] = checkAnswerResult
+      }
+
+      // TODO это условия перехода к следующему шагу
+      return Boolean(checkAnswerResult)
+    })
+  }
+}
+
+export { ConversationPart }
