c16b61616d1e8f1ee501c77fefaacf8010be86b1
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-29 18:09:12 +0300

    refactor(bot/application): mv appointment to `./appointment-conversation-service`

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment-conversation-service/appointment.conversation-service.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment-conversation-service/appointment.conversation-service.ts
@@ -1,18 +1,20 @@
+import type { AppointmentConversationDataType }     from './appointment.interfaces.js'
+
 import { Injectable }                               from '@nestjs/common'
 // TODO for what?
 import { Logger }                                   from '@nestjs/common'
 
-import { TelegramClientPort }                       from '../ports/index.js'
+import { TelegramClientPort }                       from '../../ports/index.js'
 import { CANCEL_APPOINTMENT_BUTTON_TEXT }           from './appointment.constants.js'
 import { CONTINUE_WITHOUT_COMMENTARY_BUTTON_TEXT }  from './appointment.constants.js'
 import { APPROVE_APPOINTMENT_BUTTON_TEXT }          from './appointment.constants.js'
 import { EDIT_APPOINTMENT_BUTTON_TEXT }             from './appointment.constants.js'
-import { AppointmentGetCarBodyConversationPart }    from './appointment.get-car-body.conversation-part.js'
-import { AppointmentGetCommentaryConversationPart } from './appointment.get-commentary.conversation-part.js'
-import { AppointmentGetDateConversationPart }       from './appointment.get-date.conversation-part.js'
-import { AppointmentGetRadiiConversationPart }      from './appointment.get-radii.conversation-part.js'
-import { AppointmentGetServicesConversationPart }   from './appointment.get-services.conversation-part.js'
-import { AppointmentGetTimeSlotConversationPart }   from './appointment.get-time-slot.conversation-part.js'
+import { AppointmentGetCommentaryConversationPart } from './conversation-parts/index.js'
+import { AppointmentGetDateConversationPart }       from './conversation-parts/index.js'
+import { AppointmentGetRadiiConversationPart }      from './conversation-parts/index.js'
+import { AppointmentGetServicesConversationPart }   from './conversation-parts/index.js'
+import { AppointmentGetTimeSlotConversationPart }   from './conversation-parts/index.js'
+import { AppointmentGetCarBodyConversationPart }    from './conversation-parts/index.js'
 
 @Injectable()
 export class AppointmentConversationService {
@@ -30,17 +32,20 @@ export class AppointmentConversationService {
   async process(ctx: any) {
     try {
       // TODO type
-      const appointmentData: Record<string, any> = {}
+      // const appointmentData: Record<string, any> = {}
       await this.telegramClient.sendMessage(ctx, 'Начало диалога в use case')
 
-      const appointmentConversation = await this.telegramClient.createConversation(ctx)
+      const appointmentConversation: {
+        data: AppointmentConversationDataType
+      } = await this.telegramClient.createConversation(ctx)
+
       appointmentConversation.data = {}
 
       await this.appointmentGetDateConversationPart.sendQuestion(ctx)
       await appointmentConversation.wait('msg.text', (ctx) => {
         const checkAnswerResult = this.appointmentGetDateConversationPart.checkAnswer(ctx)
         if (checkAnswerResult) {
-          appointmentConversation.data.selectedDay = checkAnswerResult
+          appointmentConversation.data.selectedDate = checkAnswerResult
         }
 
         return Boolean(checkAnswerResult)
@@ -48,13 +53,13 @@ export class AppointmentConversationService {
 
       await this.appointmentGetTimeSlotConversationPart.sendQuestion(
         ctx,
-        appointmentConversation.data.selectedDay
+        appointmentConversation.data.selectedDate.milliseconds
       )
 
       await appointmentConversation.wait('msg.text', (ctx) => {
         const checkAnswerResult = this.appointmentGetTimeSlotConversationPart.checkAnswer(ctx)
         if (checkAnswerResult) {
-          appointmentConversation.data.selectedDay = checkAnswerResult
+          appointmentConversation.data.selectedTimeSlot = checkAnswerResult
         }
 
         return Boolean(checkAnswerResult)
@@ -98,31 +103,31 @@ export class AppointmentConversationService {
 
       await appointmentConversation.wait('msg.text', (ctx) => {
         const checkAnswerResult = this.appointmentGetCommentaryConversationPart.checkAnswer(ctx)
-        if (checkAnswerResult && checkAnswerResult !== ' ') {
-          appointmentConversation.data.selectedRadii = checkAnswerResult
+        if (typeof checkAnswerResult === 'string' && checkAnswerResult !== ' ') {
+          appointmentConversation.data.commentary = checkAnswerResult
         }
 
         return Boolean(checkAnswerResult)
       })
 
-      console.log(appointmentData)
+      console.log(appointmentConversation.data)
 
-      // const { carBodyTitle, radiiTitle, serviceTitle, commentary, selectedTimeSlot } =
-      //   appointmentData
+      const { selectedCarBody, selectedRadii, selectedService, commentary, selectedTimeSlot } =
+        appointmentConversation.data
 
-      // const selectedTimeDate = new Date(selectedTimeSlot.milliseconds)
-      // let selectedDateText = selectedTimeDate.toLocaleDateString('ru-RU', {
-      //   weekday: 'long',
-      //   month: 'long',
-      //   day: 'numeric',
-      //   hour: '2-digit',
-      //   minute: '2-digit',
-      // })
+      const selectedTimeDate = new Date(selectedTimeSlot.milliseconds)
+      let selectedDateText = selectedTimeDate.toLocaleDateString('ru-RU', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      })
 
       let approvalMessage = ''
-      approvalMessage += `Тип кузова: ${carBodyTitle}\n`
-      approvalMessage += `Диаметр колёс: ${radiiTitle}\n`
-      approvalMessage += `Тип ремонта: ${serviceTitle}\n`
+      approvalMessage += `Тип кузова: ${selectedCarBody}\n`
+      approvalMessage += `Диаметр колёс: ${selectedRadii}\n`
+      approvalMessage += `Тип ремонта: ${selectedService}\n`
       approvalMessage += `Выбранная дата: ${selectedDateText}\n`
       if (commentary) approvalMessage += `Комментарий: ${commentary}`
 
@@ -158,7 +163,7 @@ export class AppointmentConversationService {
         'ссылки на оператора (другой чат), скрыть клавиатуру'
       )
 
-      this.telegramClient.removeappointment(ctx)
+      this.telegramClient.removeConversation(ctx)
     } catch (error) {
       // TODO что за класс Logger
       // если он пишет логи, то в случае ошибок нужно использовать его
