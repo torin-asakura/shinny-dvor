eb860f85d715fe7fa36f8c86f4ea60f3f2415474
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-01 16:26:52 +0300

    feat(query-client): add types & todo comment

--- booking-telegram-bot/contexts/query-client/application-module/src/ports/query-client.port.ts
+++ booking-telegram-bot/contexts/query-client/application-module/src/ports/query-client.port.ts
@@ -1,3 +1,10 @@
+// TODO
+// import type from adapter???
+// import that types to adapter!
+
 export abstract class QueryClientPort {
-  abstract runQuery(query)
+  abstract getCarBodyTitles(): Promise<Array<string>>
+  abstract getRadiiTitles(): Promise<Array<string>>
+  abstract getServiceTitles(): Promise<Array<string>>
+  abstract getWorkTimeRawString(): Promise<string>
 }
