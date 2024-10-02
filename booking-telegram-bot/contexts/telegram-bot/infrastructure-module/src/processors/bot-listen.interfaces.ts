0dd9a1657e782ee8e805e7827bc60ee82a297fc3
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-02 12:36:24 +0300

    refactor(telegram-bot-context/infrastructure): extend formatted-context interface

--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/processors/bot-listen.interfaces.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/processors/bot-listen.interfaces.ts
@@ -2,6 +2,10 @@ type TelegramBotFormattedContextType = {
   userId: bigint
   accessHash: bigint
   messageId: number
+  chatId: bigint
 }
 
+type TelegramBotFormattedContextKeyType = keyof TelegramBotFormattedContextType
+
 export type { TelegramBotFormattedContextType }
+export type { TelegramBotFormattedContextKeyType }
