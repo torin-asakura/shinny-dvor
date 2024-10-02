9660a444d56ab96dcc34a3257c9f5a18a28b60ad
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-02 12:34:40 +0300

    refactor(telegram-bot-context/application): comment all login; WIP

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment-conversation/appointment-conversation.service.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/services/appointment-conversation/appointment-conversation.service.ts
@@ -25,45 +25,51 @@ export class AppointmentConversationService {
     private readonly appointmentGetApprovalConversationPart: AppointmentGetApprovalConversationPart
   ) {}
 
+  // TODO it is must be formatted-telegram-bot-context, cause context layer
   async process(ctx) {
     try {
       await this.telegramClient.sendMessage(
         ctx,
+        // TODO start appointment-conversation-message
         ruLocale.appointmentConversation.startConversationMessage
       )
 
+      // TODO get-conversation
+      // TODO what is conversation?
       const appointmentConversation: {
         data: AppointmentConversationDataType
       } = await this.telegramClient.createConversation(ctx)
 
+      console.log(appointmentConversation)
+
       appointmentConversation.data = {}
 
-      await this.appointmentGetDateConversationPart.process(ctx, appointmentConversation)
-
-      const selectedDateMs = appointmentConversation.data.date.milliseconds
-      await this.appointmentGetTimeSlotConversationPart.process(ctx, appointmentConversation, {
-        questionData: selectedDateMs,
-      })
-
-      await this.appointmentGetCarBodyConversationPart.process(ctx, appointmentConversation)
-      await this.appointmentGetRadiiConversationPart.process(ctx, appointmentConversation)
-      await this.appointmentGetServicesConversationPart.process(ctx, appointmentConversation)
-
-      await this.appointmentGetCommentaryConversationPart.process(ctx, appointmentConversation)
-
-      await this.appointmentGetApprovalConversationPart.process(ctx, appointmentConversation, {
-        questionData: appointmentConversation.data,
-      })
-
-      // TODO write to DB - appointmentConversation.data
-      // TODO write with telegram-client-data (phone - if available, userId )
-
-      await this.telegramClient.sendMessage(
-        ctx,
-        ruLocale.appointmentConversation.endConversatoinMessage
-      )
-
-      this.telegramClient.removeConversation(ctx)
+      //   await this.appointmentGetDateConversationPart.process(ctx, appointmentConversation)
+      //
+      //   const selectedDateMs = appointmentConversation.data.date.milliseconds
+      //   await this.appointmentGetTimeSlotConversationPart.process(ctx, appointmentConversation, {
+      //     questionData: selectedDateMs,
+      //   })
+      //
+      //   await this.appointmentGetCarBodyConversationPart.process(ctx, appointmentConversation)
+      //   await this.appointmentGetRadiiConversationPart.process(ctx, appointmentConversation)
+      //   await this.appointmentGetServicesConversationPart.process(ctx, appointmentConversation)
+      //
+      //   await this.appointmentGetCommentaryConversationPart.process(ctx, appointmentConversation)
+      //
+      //   await this.appointmentGetApprovalConversationPart.process(ctx, appointmentConversation, {
+      //     questionData: appointmentConversation.data,
+      //   })
+      //
+      //   // TODO write to DB - appointmentConversation.data
+      //   // TODO write with telegram-client-data (phone - if available, userId )
+      //
+      //   await this.telegramClient.sendMessage(
+      //     ctx,
+      //     ruLocale.appointmentConversation.endConversatoinMessage
+      //   )
+      //
+      //   this.telegramClient.removeConversation(ctx)
     } catch (error) {
       console.error(error)
       const { serverErrorMessage } = ruLocale.appointmentConversation
