b8be14a45c65eb9fe8b4f1717028a382afa56ce9
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-02 12:36:44 +0300

    refactor(telegram-bot-context/infrastructure): update processor namings

--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/processors/bot-listen.processor.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/processors/bot-listen.processor.ts
@@ -1,38 +1,44 @@
 import { Injectable }                     from '@nestjs/common'
 
 import { TgsnakeAdapterService }          from '@booking-telegram-bot/tgsnake-adapter'
-import { StartCommandUseCase }            from '@telegram-bot/application-module'
-import { HelpCommandUseCase }             from '@telegram-bot/application-module'
-import { AppointmentConversationUseCase } from '@telegram-bot/application-module'
+import { StartCommand }                   from '@telegram-bot/application-module'
+import { HelpCommand }                    from '@telegram-bot/application-module'
+import { AppointmentConversationCommand } from '@telegram-bot/application-module'
 import { ReceiveMessageUseCase }          from '@telegram-bot/application-module'
 
 @Injectable()
 export class BotListenProcessor {
   constructor(
     private readonly telegramClient: TgsnakeAdapterService,
-    private readonly startCommandUseCase: StartCommandUseCase,
-    private readonly helpCommandUseCase: HelpCommandUseCase,
-    private readonly appointmentConversationUseCase: AppointmentConversationUseCase,
-    private readonly receiveMessageService: ReceiveMessageUseCase
+    private readonly startCommand: StartCommand,
+    private readonly helpCommand: HelpCommand,
+    private readonly appointmentConversationCommand: AppointmentConversationCommand,
+    private readonly receiveMessageUseCase: ReceiveMessageUseCase
   ) {}
 
-  async process() {
+  async processCommand_startCommand() {
+    this.telegramClient.onCommand('start', async (ctx) => {
+      await this.startCommand.execute(ctx)
+    })
+  }
+
+  async processCommand_helpCommand() {
+    this.telegramClient.onCommand('help', async (ctx) => {
+      await this.helpCommand.execute(ctx)
+    })
+  }
+
+  async processCommand_createAppointment() {
+    this.telegramClient.onCommand('create_appointment', async (ctx) => {
+      await this.appointmentConversationCommand.process(ctx)
+    })
+  }
+
+  async processReceiveMessage() {
     // TODO что такое ctx для этого уровня???
     // его нужно переопределить - унифицировать. для того, чтобы передавать по всему приложению
-    this.telegramClient.onCommand('start', async (ctx) => {
-      await this.startCommandUseCase.execute(ctx)
-    })
-
-    this.telegramClient.onCommand('help', async (ctx) => {
-      await this.helpCommandUseCase.execute(ctx)
-    })
-
-    this.telegramClient.onCommand('create_appointment', async (ctx) => {
-      await this.appointmentConversationUseCase.process(ctx)
-    })
-
     this.telegramClient.onMessage(async (ctx) => {
-      await this.receiveMessageService.process(ctx)
+      await this.receiveMessageUseCase.process(ctx)
     })
   }
 }
