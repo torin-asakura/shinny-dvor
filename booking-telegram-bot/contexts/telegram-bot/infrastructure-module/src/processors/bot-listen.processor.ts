a695d5f40e4ff1448e91275525812bdba5a6e235
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-08 10:44:44 +0300

    feat(bot/infrastructure): add onModuleInit func

--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/processors/bot-listen.processor.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/processors/bot-listen.processor.ts
@@ -1,3 +1,5 @@
+import type { OnModuleInit }              from '@nestjs/common'
+
 import { Injectable }                     from '@nestjs/common'
 
 import { TgsnakeAdapterService }          from '@booking-telegram-bot/tgsnake-adapter'
@@ -9,7 +11,7 @@ import { ReceiveMessageUseCase }          from '@telegram-bot/application-module
 // TODO divide that class into each command personal processor
 
 @Injectable()
-export class BotListenProcessor {
+export class BotListenProcessor implements OnModuleInit {
   constructor(
     private readonly telegramClient: TgsnakeAdapterService,
     private readonly startCommand: StartCommand,
@@ -18,25 +20,32 @@ export class BotListenProcessor {
     private readonly receiveMessageUseCase: ReceiveMessageUseCase
   ) {}
 
-  async processCommand_startCommand(): Promise<void> {
+  async onModuleInit(): Promise<void> {
+    await this.processCommand_startCommand()
+    await this.processCommand_helpCommand()
+    await this.processCommand_createAppointment()
+    await this.processReceiveMessage()
+  }
+
+  private async processCommand_startCommand(): Promise<void> {
     this.telegramClient.onCommand('start', async (ctx) => {
       await this.startCommand.execute(ctx)
     })
   }
 
-  async processCommand_helpCommand(): Promise<void> {
+  private async processCommand_helpCommand(): Promise<void> {
     this.telegramClient.onCommand('help', async (ctx) => {
       await this.helpCommand.execute(ctx)
     })
   }
 
-  async processCommand_createAppointment(): Promise<void> {
+  private async processCommand_createAppointment(): Promise<void> {
     this.telegramClient.onCommand('create_appointment', async (ctx) => {
       await this.appointmentConversationCommand.process(ctx)
     })
   }
 
-  async processReceiveMessage(): Promise<void> {
+  private async processReceiveMessage(): Promise<void> {
     this.telegramClient.onMessage(async (ctx) => {
       await this.receiveMessageUseCase.process(ctx)
     })
