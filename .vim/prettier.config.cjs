2779fc976f9c5ef62d44709159b3d5b154b02698
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-08 09:09:21 +0300

    refactor(common): rm .vim

--- .vim/prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [require('@atls/prettier-plugin')],
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  jsxSingleQuote: true,
-  trailingComma: 'es5',
-  printWidth: 100,
-}
