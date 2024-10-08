c7fc5e7dc711f493b9ddc81b7f8bd1f7f0d4d0e0
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-08 10:36:24 +0300

    feat(apollo-adapter): add config-uri fallback

--- booking-telegram-bot/adapters/apollo-adapter/src/client/apollo.client.ts
+++ booking-telegram-bot/adapters/apollo-adapter/src/client/apollo.client.ts
@@ -2,7 +2,7 @@ import { ApolloClient }  from '@apollo/client/core'
 import { InMemoryCache } from '@apollo/client/core'
 
 const client = new ApolloClient({
-  uri: 'https://wp.shdvor.pro/graphql',
+  uri: process.env.GRAPHQL_API_URL || 'https://wp.shdvor.pro/graphql',
   cache: new InMemoryCache(),
 })
 
