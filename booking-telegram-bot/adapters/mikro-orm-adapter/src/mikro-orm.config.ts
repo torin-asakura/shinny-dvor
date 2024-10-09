027ff0efea2dab4a2cce8a484090e2785a5e4059
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-09 12:44:00 +0300

    feat(mikro-orm-adapter): mikro-ork.config WIP

---
+++ booking-telegram-bot/adapters/mikro-orm-adapter/src/mikro-orm.config.ts
@@ -0,0 +1,38 @@
+import { Migrator }         from '@mikro-orm/migrations'
+import { PostgreSqlDriver } from '@mikro-orm/postgresql'
+import { defineConfig }     from '@mikro-orm/postgresql'
+
+import * as entities        from './entities/index.js'
+
+// import * as migrations      from './migrations'
+
+const mikroOrmOptions = defineConfig({
+  // TODO port to consts
+  port: 5432,
+  host: process.env.DB_HOST ?? 'localhost',
+  extensions: [Migrator],
+  dbName: process.env.DB_DATABASE ?? 'db',
+  user: process.env.DB_USERNAME ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'password',
+  driver: PostgreSqlDriver,
+  entities: Object.values(entities),
+  entitiesTs: Object.values(entities),
+  debug: process.env.NODE_ENV !== 'production',
+  // migrations: {
+  //   migrationsList: [
+  //     {
+  //       name: 'Migration20240524105900.ts',
+  //       class: migrations.Migration20240524105900,
+  //     },
+  //   ],
+  //   snapshot: false,
+  //   allOrNothing: true,
+  //   safe: true,
+  //   dropTables: false,
+  //   transactional: true,
+  //   emit: 'ts',
+  // },
+  forceUndefined: true,
+})
+
+export default mikroOrmOptions
