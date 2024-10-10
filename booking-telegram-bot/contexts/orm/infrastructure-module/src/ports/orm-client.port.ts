cd372d512df68f36868eb21e6d2f7bb9ea62d1a7
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-10 17:52:31 +0300

    feat(orm/infrastructure): port wip

--- booking-telegram-bot/contexts/orm/infrastructure-module/src/ports/orm-client.port.ts
+++ booking-telegram-bot/contexts/orm/infrastructure-module/src/ports/orm-client.port.ts
@@ -1,11 +1,10 @@
-import type { OrmPort }         from '@orm-client/application-module'
-
-import { MikroORM }             from '@mikro-orm/core'
-import { CreateRequestContext } from '@mikro-orm/core'
-import { Injectable }           from '@nestjs/common'
-
 // import { AppointmentDataType }  from '@booking-telegram-bot/mikro-orm-adapter'
-import { AppointmentEntity }    from '@booking-telegram-bot/mikro-orm-adapter'
+import type { AppointmentEntity } from '@booking-telegram-bot/mikro-orm-adapter'
+import type { OrmPort }           from '@orm-client/application-module'
+
+import { MikroORM }               from '@mikro-orm/core'
+import { CreateRequestContext }   from '@mikro-orm/core'
+import { Injectable }             from '@nestjs/common'
 
 // TODO из адаптера идет импорт ентити и тут используется
 // https://github.com/candify-tech/web/blob/a7221ac09da065a378af652203bfad5a1f0dbca3/applicant/server/core/infrastructure-module/src/repositories/applicant.repository.ts
@@ -15,14 +14,15 @@ class OrmPortimpl implements OrmPort {
   constructor(private readonly orm: MikroORM) {}
 
   @CreateRequestContext()
-  // async writeAppointmentData(appointmentData: AppointmentDataType): Promise<void> {
-  async writeAppointmentData(): Promise<void> {
-    const appointment = Object.assign(new AppointmentEntity(), {
-      id: 1234,
-      telegramUserId: BigInt(123),
+  // TODO
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  async writeAppointmentData(appointmentData: any): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    this.orm.em.create<AppointmentEntity>('AppointmentEntity', {
+      ...appointmentData,
+      timeSlot: 12345,
     })
 
-    this.orm.em.persist(appointment)
     await this.orm.em.flush()
   }
 }
