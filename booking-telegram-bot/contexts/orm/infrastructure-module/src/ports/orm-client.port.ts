6aa40a78aef6d0f39621d7999d51920405936f7b
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-11 10:09:51 +0300

    refactor(orm/infrastructure): clean comments and mock-data

--- booking-telegram-bot/contexts/orm/infrastructure-module/src/ports/orm-client.port.ts
+++ booking-telegram-bot/contexts/orm/infrastructure-module/src/ports/orm-client.port.ts
@@ -1,26 +1,21 @@
-// import { AppointmentDataType }  from '@booking-telegram-bot/mikro-orm-adapter'
 import type { AppointmentEntity } from '@booking-telegram-bot/mikro-orm-adapter'
 import type { OrmPort }           from '@orm-client/application-module'
 
+import { RequiredEntityData }     from '@mikro-orm/core'
 import { MikroORM }               from '@mikro-orm/core'
 import { CreateRequestContext }   from '@mikro-orm/core'
 import { Injectable }             from '@nestjs/common'
-
-// TODO из адаптера идет импорт ентити и тут используется
-// https://github.com/candify-tech/web/blob/a7221ac09da065a378af652203bfad5a1f0dbca3/applicant/server/core/infrastructure-module/src/repositories/applicant.repository.ts
 
 @Injectable()
 class OrmPortimpl implements OrmPort {
   constructor(private readonly orm: MikroORM) {}
 
   @CreateRequestContext()
-  // TODO
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  async writeAppointmentData(appointmentData: any): Promise<void> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  async writeAppointmentData(
+    appointmentData: RequiredEntityData<AppointmentEntity>
+  ): Promise<void> {
     this.orm.em.create<AppointmentEntity>('AppointmentEntity', {
       ...appointmentData,
-      timeSlot: 12345,
     })
 
     await this.orm.em.flush()
