c33754840babb4d22efce03fcff08612837d1675
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-15 17:59:49 +0300

    refactor(query-client/infrastructure): use use-case interfaces

--- booking-telegram-bot/contexts/query-client/infrastructure-module/src/ports/query-client.port.ts
+++ booking-telegram-bot/contexts/query-client/infrastructure-module/src/ports/query-client.port.ts
@@ -1,26 +1,34 @@
-import type { QueryClientPort } from '@query-client/application-module'
+import type { QueryClientPort }        from '@query-client/application-module'
 
-import { Injectable }           from '@nestjs/common'
+import { Injectable }                  from '@nestjs/common'
 
-import { ApolloAdapterService } from '@booking-telegram-bot/apollo-adapter'
+import { GetCarBodyTitlesUseCase }     from '@booking-telegram-bot/apollo-adapter'
+import { GetRadiiTitlesUseCase }       from '@booking-telegram-bot/apollo-adapter'
+import { GetServiceTitlesUseCase }     from '@booking-telegram-bot/apollo-adapter'
+import { GetWorkTimeRawStringUseCase } from '@booking-telegram-bot/apollo-adapter'
 
 @Injectable()
 export class QueryClientPortImpl implements QueryClientPort {
-  constructor(private readonly queryClient: ApolloAdapterService) {}
+  constructor(
+    private readonly getCarBodyTitlesUseCase: GetCarBodyTitlesUseCase,
+    private readonly getRadiiTitlesUseCase: GetRadiiTitlesUseCase,
+    private readonly getServiceTitlesUseCase: GetServiceTitlesUseCase,
+    private readonly getWorkTimeRawStringUseCase: GetWorkTimeRawStringUseCase
+  ) {}
 
   async getCarBodyTitles(): Promise<Array<string>> {
-    return this.queryClient.getCarBodyTitles()
+    return this.getCarBodyTitlesUseCase.process()
   }
 
   async getRadiiTitles(): Promise<Array<string>> {
-    return this.queryClient.getRadiiTitles()
+    return this.getRadiiTitlesUseCase.process()
   }
 
   async getServiceTitles(): Promise<Array<string>> {
-    return this.queryClient.getServiceTitles()
+    return this.getServiceTitlesUseCase.process()
   }
 
   async getWorkTimeRawString(): Promise<string> {
-    return this.queryClient.getWorkTimeRawString()
+    return this.getWorkTimeRawStringUseCase.process()
   }
 }
