fbcca2feb908aa270ccbff9f50715e8e8a849868
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-20 12:17:23 +0300

    refactor(tgsnake-adapter): change Error to TgsnakeAdapterError

--- booking-telegram-bot/adapters/tgsnake-adapter/src/helpers/check-formatted-context.helper.ts
+++ booking-telegram-bot/adapters/tgsnake-adapter/src/helpers/check-formatted-context.helper.ts
@@ -1,13 +1,15 @@
 import type { TelegramBotFormattedContextKeyType } from '@booking-telegram-bot/tgsnake-adapter'
 import type { TelegramBotFormattedContextType }    from '@booking-telegram-bot/tgsnake-adapter'
 
+import { TgsnakeAdapterError }                     from '@booking-telegram-bot/tgsnake-adapter'
+
 const checkFormattedContextHelper = (
   formattedContext: Record<TelegramBotFormattedContextKeyType, any>
 ): TelegramBotFormattedContextType => {
   const formattedContextKeys = Object.keys(formattedContext)
   for (const contextKey of formattedContextKeys) {
     if (!formattedContext[contextKey as TelegramBotFormattedContextKeyType]) {
-      throw new Error(`cannot access ${contextKey}`)
+      throw new TgsnakeAdapterError(`cannot access ${contextKey}`)
     }
   }
   return formattedContext
