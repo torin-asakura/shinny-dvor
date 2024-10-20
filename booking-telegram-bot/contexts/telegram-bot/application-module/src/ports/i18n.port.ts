c397ecaa214bc244588382f4b5a941ca60024c68
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-20 17:55:33 +0300

    feat(bot-ports): add appointment-conversatoin-i18n methods

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/ports/i18n.port.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/ports/i18n.port.ts
@@ -6,4 +6,44 @@ export abstract class I18nPort {
   abstract getCanceled(): string
 
   abstract getEntrypoint(): string
+
+  abstract getAppointmentConversationServerError(): string
+
+  abstract getAppointmentConversationStartConversation(): string
+
+  abstract getAppointmentConversationEndConversation(): string
+
+  abstract getAppointmentConversationCarBodyTitle(): string
+
+  abstract getAppointmentConversationRadiiTitle(): string
+
+  abstract getAppointmentConversationServiceTitle(): string
+
+  abstract getAppointmentConversationSelectedDateTitle(): string
+
+  abstract getAppointmentConversationCommentaryTitle(): string
+
+  abstract getAppointmentConversationApproveAppointmentButton(): string
+
+  abstract getAppointmentConversationEditAppointmentButton(): string
+
+  abstract getAppointmentConversationSelectDateMessage(): string
+
+  abstract getAppointmentConversationSelectCarBodyMessage(): string
+
+  abstract getAppointmentConversationSelectRadiiMessage(): string
+
+  abstract getAppointmentConversationSelectServiceMessage(): string
+
+  abstract getAppointmentConversationSelectTimeSlotMessage(): string
+
+  abstract getAppointmentConversationCancelAppointmentButton(): string
+
+  abstract getAppointmentConversationCancelAppointmentCommand(): string
+
+  abstract getAppointmentConversationMissClick(): string
+
+  abstract getAppointmentConversationContinueWithoutCommentaryButton(): string
+
+  abstract getAppointmentConversationClosedTimeSlotMessage(): string
 }
--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/ports/i18n.port.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/ports/i18n.port.ts
@@ -23,6 +23,86 @@ class I18nPortImpl implements I18nPort {
   getEntrypoint(): string {
     return this.i18n.getEntrypoint()
   }
+
+  getAppointmentConversationServerError(): string {
+    return this.i18n.getAppointmentConversationServerError()
+  }
+
+  getAppointmentConversationStartConversation(): string {
+    return this.i18n.getAppointmentConversationStartConversation()
+  }
+
+  getAppointmentConversationEndConversation(): string {
+    return this.i18n.getAppointmentConversationEndConversation()
+  }
+
+  getAppointmentConversationCarBodyTitle(): string {
+    return this.i18n.getAppointmentConversationCarBodyTitle()
+  }
+
+  getAppointmentConversationRadiiTitle(): string {
+    return this.i18n.getAppointmentConversationRadiiTitle()
+  }
+
+  getAppointmentConversationServiceTitle(): string {
+    return this.i18n.getAppointmentConversationServiceTitle()
+  }
+
+  getAppointmentConversationSelectedDateTitle(): string {
+    return this.i18n.getAppointmentConversationSelectedDateTitle()
+  }
+
+  getAppointmentConversationCommentaryTitle(): string {
+    return this.i18n.getAppointmentConversationCommentaryTitle()
+  }
+
+  getAppointmentConversationApproveAppointmentButton(): string {
+    return this.i18n.getAppointmentConversationApproveAppointmentButton()
+  }
+
+  getAppointmentConversationEditAppointmentButton(): string {
+    return this.i18n.getAppointmentConversationEditAppointmentButton()
+  }
+
+  getAppointmentConversationSelectDateMessage(): string {
+    return this.i18n.getAppointmentConversationSelectDateMessage()
+  }
+
+  getAppointmentConversationSelectCarBodyMessage(): string {
+    return this.i18n.getAppointmentConversationSelectCarBodyMessage()
+  }
+
+  getAppointmentConversationSelectRadiiMessage(): string {
+    return this.i18n.getAppointmentConversationSelectRadiiMessage()
+  }
+
+  getAppointmentConversationSelectServiceMessage(): string {
+    return this.i18n.getAppointmentConversationSelectServiceMessage()
+  }
+
+  getAppointmentConversationSelectTimeSlotMessage(): string {
+    return this.i18n.getAppointmentConversationSelectTimeSlotMessage()
+  }
+
+  getAppointmentConversationCancelAppointmentButton(): string {
+    return this.i18n.getAppointmentConversationCancelAppointmentButton()
+  }
+
+  getAppointmentConversationCancelAppointmentCommand(): string {
+    return this.i18n.getAppointmentConversationCancelAppointmentCommand()
+  }
+
+  getAppointmentConversationMissClick(): string {
+    return this.i18n.getAppointmentConversationMissClick()
+  }
+
+  getAppointmentConversationContinueWithoutCommentaryButton(): string {
+    return this.i18n.getAppointmentConversationContinueWithoutCommentaryButton()
+  }
+
+  getAppointmentConversationClosedTimeSlotMessage(): string {
+    return this.i18n.getAppointmentConversationClosedTimeSlotMessage()
+  }
 }
 
 export { I18nPortImpl }
