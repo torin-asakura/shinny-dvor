ef27369967634b28e059520c0ac167b834257a44
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-20 14:16:23 +0300

    feat(bot/infrastructure): add i18n to providers

--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/module/telegram-bot.infrastructure.providers.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/module/telegram-bot.infrastructure.providers.ts
@@ -1,14 +1,20 @@
 import type { Provider }          from '@nestjs/common'
 
 import { TelegramClientPort }     from '@telegram-bot/application-module'
+import { I18nPort }               from '@telegram-bot/application-module'
 
 import * as processors            from '../processors/index.js'
 import { TelegramClientPortImpl } from '../ports/index.js'
+import { I18nPortImpl }           from '../ports/index.js'
 
 export const telegramBotProviders: Array<Provider> = [
   {
     provide: TelegramClientPort,
     useClass: TelegramClientPortImpl,
+  },
+  {
+    provide: I18nPort,
+    useClass: I18nPortImpl,
   },
   ...Object.values(processors),
 ]
