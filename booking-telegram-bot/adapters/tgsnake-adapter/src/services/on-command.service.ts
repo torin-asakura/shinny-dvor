c89a2489bfcc93c83fb9eed5d91b172103a21bb2
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-21 18:22:08 +0300

    refactor(tgsnake-adapter): naming

--- booking-telegram-bot/adapters/tgsnake-adapter/src/services/on-command.service.ts
+++ booking-telegram-bot/adapters/tgsnake-adapter/src/services/on-command.service.ts
@@ -7,7 +7,7 @@ import { TgsnakeAdapterService }     from './index.js'
 import { getFormattedContextGetter } from '../getters/index.js'
 
 @Injectable()
-class OnCommandService {
+class OnTgsnakeCommandService {
   constructor(private readonly tgsnakeAdapterService: TgsnakeAdapterService) {}
 
   process(command: string, callback: CallbackType): OnCommandReturnType {
@@ -18,4 +18,4 @@ class OnCommandService {
   }
 }
 
-export { OnCommandService }
+export { OnTgsnakeCommandService }
