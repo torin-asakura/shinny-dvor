e4e61f44fb66af96cd665f5c664468aec01076a1
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-22 16:44:45 +0300

    refactor(entrypoint): left only infra-modules

--- booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
+++ booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
@@ -1,16 +1,9 @@
 import { Module }                          from '@nestjs/common'
 
-import { QueryClientApplicationModule }    from '@query-client/application-module'
 import { QueryClientInfrastructureModule } from '@query-client/infrastructure-module'
-import { TelegramBotApplicationModule }    from '@telegram-bot/application-module'
 import { TelegramBotInfrastructureModule } from '@telegram-bot/infrastructure-module'
 
 @Module({
-  imports: [
-    TelegramBotApplicationModule.register(),
-    TelegramBotInfrastructureModule.register(),
-    QueryClientApplicationModule.register(),
-    QueryClientInfrastructureModule.register(),
-  ],
+  imports: [TelegramBotInfrastructureModule.register(), QueryClientInfrastructureModule.register()],
 })
 export class BotServiceEntrypointModule {}
