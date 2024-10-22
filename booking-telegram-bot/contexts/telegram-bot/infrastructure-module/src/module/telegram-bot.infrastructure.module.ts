165573c8fb6f04f760ac6fd4906bd494fe6f08f1
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-22 12:00:24 +0300

    refactor: move adapter to infra level (from entrypoint)

--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/module/telegram-bot.infrastructure.module.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/module/telegram-bot.infrastructure.module.ts
@@ -2,6 +2,9 @@ import type { DynamicModule }           from '@nestjs/common'
 
 import { Module }                       from '@nestjs/common'
 
+import { I18nAdapterModule }            from '@booking-telegram-bot/i18n-adapter'
+import { MikroOrmAdapterModule }        from '@booking-telegram-bot/mikro-orm-adapter'
+import { TgsnakeAdapterModule }         from '@booking-telegram-bot/tgsnake-adapter'
 import { TelegramBotApplicationModule } from '@telegram-bot/application-module'
 
 import { telegramBotProviders }         from './telegram-bot.infrastructure.providers.js'
@@ -16,7 +19,12 @@ export class TelegramBotInfrastructureModule {
       module: TelegramBotInfrastructureModule,
       providers: [...providers],
       exports: [...providers],
-      imports: [TelegramBotApplicationModule.register()],
+      imports: [
+        I18nAdapterModule.register(),
+        MikroOrmAdapterModule.register(),
+        TgsnakeAdapterModule.register(),
+        TelegramBotApplicationModule.register(),
+      ],
     }
   }
 }
--- booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
+++ booking-telegram-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
@@ -1,9 +1,5 @@
 import { Module }                          from '@nestjs/common'
 
-import { ApolloAdapterModule }             from '@booking-telegram-bot/apollo-adapter'
-import { I18nAdapterModule }               from '@booking-telegram-bot/i18n-adapter'
-import { MikroOrmAdapterModule }           from '@booking-telegram-bot/mikro-orm-adapter'
-import { TgsnakeAdapterModule }            from '@booking-telegram-bot/tgsnake-adapter'
 import { QueryClientApplicationModule }    from '@query-client/application-module'
 import { QueryClientInfrastructureModule } from '@query-client/infrastructure-module'
 import { TelegramBotApplicationModule }    from '@telegram-bot/application-module'
@@ -11,14 +7,10 @@ import { TelegramBotInfrastructureModule } from '@telegram-bot/infrastructure-mo
 
 @Module({
   imports: [
-    TgsnakeAdapterModule.register(),
     TelegramBotApplicationModule.register(),
     TelegramBotInfrastructureModule.register(),
-    ApolloAdapterModule.register(),
     QueryClientApplicationModule.register(),
     QueryClientInfrastructureModule.register(),
-    MikroOrmAdapterModule.register(),
-    I18nAdapterModule.register(),
   ],
 })
 export class BotServiceEntrypointModule {}
