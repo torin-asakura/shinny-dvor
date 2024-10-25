c4a3adf9d0bc0360337cdd2ea11d6765cfdc791e
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-25 14:59:13 +0300

    feat(site/booking-api): add env to booking port

--- site/api/booking/src/booking.api.ts
+++ site/api/booking/src/booking.api.ts
@@ -1,8 +1,10 @@
 export const bookingApiHandle = async (request: Request): Promise<Response> => {
   const jsonData = await request.json()
 
-  // TODO port to envs
-  return fetch('http://localhost:3000/booking', {
+  const bookingTelegramBotPort = process.env.BOOKING_TELEGRAM_BOT_PORT || 3000
+  const fetchUrl = `http://localhost:${bookingTelegramBotPort}/booking`
+
+  return fetch(fetchUrl, {
     method: 'post',
     body: JSON.stringify(jsonData),
   })
