e572073fd2052e1883ddcd92dc0be1a069114c5f
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-25 16:17:42 +0300

    refactor(site): split booking-api-fetch-adress into 2 envs

--- site/api/booking/src/booking.api.ts
+++ site/api/booking/src/booking.api.ts
@@ -1,8 +1,10 @@
 export const bookingApiHandle = async (request: Request): Promise<Response> => {
   const jsonData = await request.json()
 
+  const bookingTelegramBotHostname = process.env.BOOKING_TELEGRAM_BOT_HOSTNAME || 'http://localhost'
   const bookingTelegramBotPort = process.env.BOOKING_TELEGRAM_BOT_PORT || 3000
-  const fetchUrl = `http://localhost:${bookingTelegramBotPort}/booking`
+
+  const fetchUrl = `${bookingTelegramBotHostname}:${bookingTelegramBotPort}/booking`
 
   return fetch(fetchUrl, {
     method: 'post',
