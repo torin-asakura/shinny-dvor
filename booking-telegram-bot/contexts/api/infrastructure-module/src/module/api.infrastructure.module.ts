fdb18b95fec71a1d44834f95fa0fa771c22f4795
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-25 13:43:12 +0300

    feat(api-infrastructure): add services to module

--- booking-telegram-bot/contexts/api/infrastructure-module/src/module/api.infrastructure.module.ts
+++ booking-telegram-bot/contexts/api/infrastructure-module/src/module/api.infrastructure.module.ts
@@ -3,18 +3,22 @@ import type { DynamicModule } from '@nestjs/common'
 import { Module }             from '@nestjs/common'
 
 import * as allControllers    from '../controllers/index.js'
+import * as services          from '../services/index.js'
 
 @Module({})
 export class ApiInfrastructureModule {
   static register(): DynamicModule {
     const controllerProviders = Object.values(allControllers)
+    const serviceProviders = Object.values(services)
 
     const controllers = [...controllerProviders]
+    const providers = [...serviceProviders]
 
     return {
       global: true,
       module: ApiInfrastructureModule,
       controllers,
+      providers,
     }
   }
 }
