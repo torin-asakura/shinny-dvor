b90e194ebabea5773e4c1611a1dc8ffd38ed3bfb
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-25 16:19:08 +0300

    refactor(site): namigs booking-api-fetch-adress

--- site/api/booking/src/booking.api.ts
+++ site/api/booking/src/booking.api.ts
@@ -1,10 +1,10 @@
 export const bookingApiHandle = async (request: Request): Promise<Response> => {
   const jsonData = await request.json()
 
-  const bookingTelegramBotHostname = process.env.BOOKING_TELEGRAM_BOT_HOSTNAME || 'http://localhost'
+  const bookingTelegramBotOrigin = process.env.BOOKING_TELEGRAM_BOT_ORIGIN || 'http://localhost'
   const bookingTelegramBotPort = process.env.BOOKING_TELEGRAM_BOT_PORT || 3000
 
-  const fetchUrl = `${bookingTelegramBotHostname}:${bookingTelegramBotPort}/booking`
+  const fetchUrl = `${bookingTelegramBotOrigin}:${bookingTelegramBotPort}/booking`
 
   return fetch(fetchUrl, {
     method: 'post',
