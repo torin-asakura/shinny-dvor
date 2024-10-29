bc1740c3c67719ce59b2f8279a20d1329712bfef
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-29 18:27:44 +0300

    feat(telegram-bot-context/application): cp formatted-context from booking-telegram-bot

---
+++ operator-bot/contexts/telegram-bot-context/application-module/src/interfaces/formatted-context.interface.ts
@@ -0,0 +1,11 @@
+type FormattedContextType = {
+  userId: bigint
+  userFirstName: string
+  userLastName?: string
+  messageText: string
+  accessHash: bigint
+  messageId: number
+  chatId: bigint
+}
+
+export type { FormattedContextType }
