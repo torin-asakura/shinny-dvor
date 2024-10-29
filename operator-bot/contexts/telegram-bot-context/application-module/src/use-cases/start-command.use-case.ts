8bb0e899b92218b7b5818ba09005ff72175c1347
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-29 18:38:50 +0300

    refactor(telegram-bot-context/application): start-command-use-case refactor for abstract class

--- operator-bot/contexts/telegram-bot-context/application-module/src/use-cases/start-command.use-case.ts
+++ operator-bot/contexts/telegram-bot-context/application-module/src/use-cases/start-command.use-case.ts
@@ -1,23 +1,20 @@
-// import type { TelegramBotFormattedContextType } from '@telegram-bot/application-module'
+import type { FormattedContextType }    from '../interfaces/index.js'
 
-import { Injectable }         from '@nestjs/common'
+import { Injectable }                   from '@nestjs/common'
 
-import { TelegramClientPort } from '../ports/index.js'
-
-// import { I18nPort }                             from '../ports/index.js'
+import { OperatorUseCaseAbstractClass } from '../interfaces/index.js'
+import { TelegramClientPort }           from '../ports/index.js'
+import { I18nPort }                     from '../ports/index.js'
 
 @Injectable()
-export class StartCommandUseCase {
-  constructor(
-    private readonly telegramClient: TelegramClientPort
-    // private readonly i18n: I18nPort
-  ) {}
+export class StartCommandUseCase extends OperatorUseCaseAbstractClass {
+  // eslint-disable-next-line
+  constructor(telegramClient: TelegramClientPort, i18n: I18nPort) {
+    super(telegramClient, i18n)
+  }
 
-  // async execute(ctx: TelegramBotFormattedContextType): Promise<void> {
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  async execute(ctx: any): Promise<void> {
-    // const welcomeMessage = this.i18n.welcome
-    await this.telegramClient.sendMessage(ctx, 'bla')
+  async executeForOperator(ctx: FormattedContextType): Promise<void> {
+    const welcomeMessage = this.i18n.welcome
+    return this.telegramClient.sendMessage(ctx, welcomeMessage)
   }
 }
