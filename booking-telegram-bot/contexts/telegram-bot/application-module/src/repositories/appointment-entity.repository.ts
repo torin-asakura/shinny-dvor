76c9728615a5ce1c6e4e91230fede9cc647a4a44
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-31 18:15:51 +0300

    feat(booking-telegram-bot/api-context/application): extend interface & appointment-entity-repository

--- booking-telegram-bot/contexts/telegram-bot/application-module/src/repositories/appointment-entity.repository.ts
+++ booking-telegram-bot/contexts/telegram-bot/application-module/src/repositories/appointment-entity.repository.ts
@@ -1,6 +1,8 @@
 import type { OrmAppointmentDataType } from '../interfaces/index.js'
 
 export abstract class AppointmentEntityRepository {
+  abstract getAppointments(): Promise<Array<OrmAppointmentDataType>>
+
   abstract writeData(appointmentData: OrmAppointmentDataType): Promise<void>
 
   abstract getDataByDay(dayMs: number): Promise<Array<[number, number]>>
