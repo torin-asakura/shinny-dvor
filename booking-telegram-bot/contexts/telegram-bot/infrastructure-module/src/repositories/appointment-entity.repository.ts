cd0153f1014959086d0865285465e694bca1d829
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-31 18:20:34 +0300

    feat(booking-telegram-bot/telegram-bot-context/infrastructure): get appointments method on appointment repostory

--- booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/repositories/appointment-entity.repository.ts
+++ booking-telegram-bot/contexts/telegram-bot/infrastructure-module/src/repositories/appointment-entity.repository.ts
@@ -12,6 +12,26 @@ class AppointmentEntityRepositoryImpl implements AppointmentEntityRepository {
   constructor(private readonly orm: MikroORM) {}
 
   @CreateRequestContext()
+  async getAppointments(): Promise<Array<RequiredEntityData<AppointmentEntity>>> {
+    // TODO rename method to get-today-and-tomorrow-appointments
+
+    const milliseconds = Date.now()
+
+    const todayDate = new Date(milliseconds)
+    todayDate.setHours(0, 0, 0, 0)
+
+    const todayDateMs = todayDate.getTime()
+    const tomorrowDateMs = todayDateMs + 48 * 60 * 60 * 1000
+
+    return this.orm.em.find('AppointmentEntity', {
+      timeSlotStart: {
+        $gt: BigInt(todayDateMs),
+        $lt: BigInt(tomorrowDateMs),
+      },
+    })
+  }
+
+  @CreateRequestContext()
   async writeData(appointmentData: RequiredEntityData<AppointmentEntity>): Promise<void> {
     this.orm.em.create<AppointmentEntity>('AppointmentEntity', {
       ...appointmentData,
