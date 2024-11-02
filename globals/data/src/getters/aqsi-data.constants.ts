c7b341a3889b91d6fca10dabede96dca81836170
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-03 01:08:34 +0300

    feat(globals): aqsi-getters

---
+++ globals/data/src/getters/aqsi-data.constants.ts
@@ -0,0 +1,3 @@
+export const AQSI_API_URL = 'https://api.aqsi.ru/pub/v2'
+export const GOODS_LIST_PATH = '/Goods/list'
+export const GOODS_CATEGORY_PATH = '/GoodsCategory/list'
---
+++ globals/data/src/getters/aqsi-data.interfaces.ts
@@ -0,0 +1,2 @@
+type GetAqsiDataReturnType = Array<{ id: string; group_id: string; name: string; price: number }>
+export type { GetAqsiDataReturnType }
