d16304ea967deaa71876ffc415c75c725488f8a6
Nikita Elfimov <nelfimov@mail.ru>
2024-11-22 12:15:42 +0300

    chore: trigger

--- site/app/src/app/page.ts
+++ site/app/src/app/page.ts
@@ -1,3 +1,3 @@
 export { generateMetadata } from '@site/index-page'
-export { revalidate } from '@site/index-page'
 export { default } from '@site/index-page'
+export { revalidate } from '@site/index-page'
