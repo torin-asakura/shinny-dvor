33145cbf56fd41f52a24348cb8743f9edf402022
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-28 17:14:09 +0300

    fix(prices): rm unused

--- prices/adapters/fetcher-adapter/src/exceptions/empty-yandex-data.exception.ts
+++
@@ -1,7 +0,0 @@
-import { FetcherAdapterExceptionMessage } from './exceptions.constants.js'
-
-export class EmptyYandexDataError extends Error {
-  constructor() {
-    super(FetcherAdapterExceptionMessage.DOCUMENT_IS_EMPTY)
-  }
-}
