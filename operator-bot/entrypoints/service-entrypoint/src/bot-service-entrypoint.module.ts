5df3511dac93f24bb6e0c5565b18bce0ae8dec76
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-28 20:38:21 +0300

    feat(operator-bot): register api infrastructure module on entrypoint

--- operator-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
+++ operator-bot/entrypoints/service-entrypoint/src/bot-service-entrypoint.module.ts
@@ -1,8 +1,9 @@
 import { Module }                          from '@nestjs/common'
+import { ApiInfrastructureModule }         from '@operator-bot/api-infrastructure-module'
 
 import { TelegramBotInfrastructureModule } from '@operator-bot/telegram-bot-context_infrastructure-module'
 
 @Module({
-  imports: [TelegramBotInfrastructureModule.register()],
+  imports: [TelegramBotInfrastructureModule.register(), ApiInfrastructureModule.register()],
 })
 export class BotServiceEntrypointModule {}
