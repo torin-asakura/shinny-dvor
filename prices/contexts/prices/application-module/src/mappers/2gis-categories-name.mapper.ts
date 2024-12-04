10933e11215601fbdbd7d043a530a8127664fcd8
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-04 16:12:20 +0300

    feat(prices): add mapper to change category & offer ids

---
+++ prices/contexts/prices/application-module/src/mappers/2gis-categories-name.mapper.ts
@@ -0,0 +1,31 @@
+import type { GoodsDataFormattedType }           from '../interfaces/index.js'
+import type { GoodsCategoriesDataFormattedType } from '../interfaces/index.js'
+
+export const map2gisCategoriesName = (
+  goodsData: GoodsDataFormattedType,
+  goodsCategoryData: GoodsCategoriesDataFormattedType
+): [GoodsDataFormattedType, GoodsCategoriesDataFormattedType] => {
+  const simplifiedIdSpecification: Record<string, string> = {}
+
+  const mappedGoodsCategoryData = goodsCategoryData.map((goodsCategoryDataItem, index) => {
+    const { id } = goodsCategoryDataItem
+    const indexString = (index + 1).toString()
+    simplifiedIdSpecification[id] = indexString
+    return {
+      ...goodsCategoryDataItem,
+      id: indexString,
+    }
+  })
+
+  const mappedGoodsData = goodsData.map((goodsDataItem) => {
+    if (goodsDataItem) {
+      return {
+        ...goodsDataItem,
+        group_id: simplifiedIdSpecification[goodsDataItem.group_id],
+      }
+    }
+    return null
+  })
+
+  return [mappedGoodsData, mappedGoodsCategoryData]
+}
