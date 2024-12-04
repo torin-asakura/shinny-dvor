583a6392ed8346472e5e3afc07638e61e8f53934
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-04 16:21:56 +0300

    Fix/2gis feed (#138)
    * feat(prices): add mapper to change category & offer ids
    
    * feat(common): add path to global types

--- prices/contexts/prices/application-module/src/mappers/index.ts
+++ prices/contexts/prices/application-module/src/mappers/index.ts
@@ -2,6 +2,7 @@ export * from './2gis-categories.mapper.js'
 export * from './2gis-goods-category-data.mapper.js'
 export * from './2gis-goods-data.mapper.js'
 export * from './2gis-offers.mapper.js'
+export * from './2gis-categories-name.mapper.js'
 
 export * from './yandex-categories.mapper.js'
 export * from './yandex-goods-category-data-deep.mapper.js'
--- prices/contexts/prices/application-module/src/services/get-2gis-prices-xml.service.ts
+++ prices/contexts/prices/application-module/src/services/get-2gis-prices-xml.service.ts
@@ -1,14 +1,15 @@
-import type { QueryDataType }  from '../interfaces/index.js'
+import type { QueryDataType }    from '../interfaces/index.js'
 
-import { Logger }              from '@atls/logger'
-import { Injectable }          from '@nestjs/common'
+import { Logger }                from '@atls/logger'
+import { Injectable }            from '@nestjs/common'
 
-import { formatYmlData }       from '../formatters/index.js'
-import { formantYmlDataToXml } from '../formatters/index.js'
-import { get2gisData }         from '../getters/index.js'
-import { writeXmlHelper }      from '../helpers/index.js'
-import { map2gisCategories }   from '../mappers/index.js'
-import { map2gisOffers }       from '../mappers/index.js'
+import { formatYmlData }         from '../formatters/index.js'
+import { formantYmlDataToXml }   from '../formatters/index.js'
+import { get2gisData }           from '../getters/index.js'
+import { writeXmlHelper }        from '../helpers/index.js'
+import { map2gisCategories }     from '../mappers/index.js'
+import { map2gisOffers }         from '../mappers/index.js'
+import { map2gisCategoriesName } from '../mappers/index.js'
 
 @Injectable()
 export class Get2gisPricesXmlService {
@@ -20,8 +21,13 @@ export class Get2gisPricesXmlService {
       queryData.goodsCategoryData
     )
 
-    const twoGisCategories = map2gisCategories(goodsCategoryData2gis)
-    const twoGisOffers = map2gisOffers(goodsData2gis)
+    const [mappedGoods, mappedGoodsCategories] = map2gisCategoriesName(
+      goodsData2gis,
+      goodsCategoryData2gis
+    )
+
+    const twoGisCategories = map2gisCategories(mappedGoodsCategories)
+    const twoGisOffers = map2gisOffers(mappedGoods)
     const twoGisYmlData = formatYmlData(twoGisCategories, twoGisOffers)
 
     const twoGisXml = formantYmlDataToXml(twoGisYmlData)
