c8446167aafe55ab8214a7677daa133168db78db
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-11 14:15:07 +0300

    feat(ui) generate new theme

---
+++ ui/theme/src/theme/lineHeights.ts
@@ -0,0 +1,12 @@
+export const lineHeights = {
+  "small.semiDefault": "0.7",
+  "small.default": "0.8",
+  "normal.reduced": "1",
+  "normal.semiDefault": "1.1",
+  "normal.default": "1.2",
+  "normal.semiIncreased": "1.3",
+  "normal.increased": "1.4",
+  "medium.semiDefault": "1.5",
+  "medium.default": "1.6",
+  "medium.semiIncreased": "1.8",
+};
