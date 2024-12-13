5395be6f5ccc03ac81475829f1a32d9b8b37ec71
Viktoria Dragun <aliceinapple99@gmail.com>
2024-12-13 16:56:59 +0300

    refactor(ui): text component

--- ui/text/src/index.ts
+++ ui/text/src/index.ts
@@ -1,5 +1,1 @@
-export { Space } from '@atls-ui-parts/text'
-
-export * from './text/index.js'
-export * from './ruble/index.js'
-export * from './text-ellipsis/index.js'
+export * from '@atls-ui-parts/text'
--- ui/text/src/text-ellipsis/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './text-ellipsis.component.js'
