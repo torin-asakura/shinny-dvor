48d086805ffd3c290ac345fda5166b9730445e13
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-13 16:43:39 +0300

    feat(globals): add is development prop

--- globals/data/src/helpers/check-envs.helper.ts
+++ globals/data/src/helpers/check-envs.helper.ts
@@ -1,9 +1,18 @@
 import type { CheckEnvsHelperType } from './check-envs.interface.js'
 
 import { CheckEnvsError }           from '../error/index.js'
+import { getCheckEnvsErrorMessage } from '../error/index.js'
 
-export const checkEnvsHelper: CheckEnvsHelperType = ({ applicationName, envsList }): void => {
+export const checkEnvsHelper: CheckEnvsHelperType = ({
+  applicationName,
+  envsList,
+  isDevelopment,
+}): void => {
   for (const envName of envsList) {
-    if (!process.env[envName]) throw new CheckEnvsError(applicationName, envName)
+    if (!process.env[envName]) {
+      // eslint-disable-next-line no-console
+      if (isDevelopment) console.error(getCheckEnvsErrorMessage(applicationName, envName))
+      else throw new CheckEnvsError(applicationName, envName)
+    }
   }
 }
--- globals/data/src/helpers/check-envs.interface.ts
+++ globals/data/src/helpers/check-envs.interface.ts
@@ -1,6 +1,7 @@
 type CheckEnvsHelperProps = {
   applicationName: string
   envsList: Array<string>
+  isDevelopment?: boolean
 }
 
 export type CheckEnvsHelperType = (props: CheckEnvsHelperProps) => void
