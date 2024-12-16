0e3578dff938712a4cba57042aab32abac51fc36
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-16 20:47:12 +0300

    refactor(ui): split theme button colors to personal file

--- ui/theme/src/index.ts
+++ ui/theme/src/index.ts
@@ -1,2 +1,3 @@
 export * from './theme.css.js'
 export * from './constants/index.js'
+export * from './button-colors/index.js'
--- ui/theme/src/theme.css.ts
+++ ui/theme/src/theme.css.ts
@@ -1,9 +1,11 @@
 import './global.css.js'
 
-import { createGlobalTheme } from '@vanilla-extract/css'
+import { createGlobalTheme }  from '@vanilla-extract/css'
 
-import * as theme            from './theme/index.js'
+import * as buttonColorsTheme from './button-colors/index.js'
+import * as theme             from './theme/index.js'
 
-export const GLOBAL_THEME_ID = 'default-theme'
+export const GLOBAL_THEME_ID = '#default-theme'
 
-export const vars = createGlobalTheme(`#${GLOBAL_THEME_ID}`, theme)
+export const vars = createGlobalTheme(GLOBAL_THEME_ID, theme)
+export const buttonColorsVars = createGlobalTheme(GLOBAL_THEME_ID, buttonColorsTheme)
