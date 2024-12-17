bace45fe301c28cca26bbcc0103073c0a7e405e3
Viktoria Dragun <aliceinapple99@gmail.com>
2024-12-17 18:29:47 +0300

    feat(ui): rewrite select button component

--- ui/select/src/button/button.component.tsx
+++ ui/select/src/button/button.component.tsx
@@ -1,5 +1,4 @@
 import { forwardRef }       from 'react'
-import { memo }             from 'react'
 import React                from 'react'
 
 import { ButtonProps }      from './button.interfaces.js'
@@ -7,24 +6,25 @@ import { appearanceStyles } from './button.css.js'
 import { baseStyles }       from './button.css.js'
 import { shapeStyles }      from './button.css.js'
 
-const Button = memo(
-  forwardRef<HTMLButtonElement, ButtonProps>(({ isSelected, onClick, children, ...props }, ref) => {
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault()
-      if (onClick) onClick(event)
-    }
+const Button = forwardRef<HTMLButtonElement, ButtonProps>((
+  { isSelected, onClick, children, ...props },
+  ref
+) => {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    if (onClick) onClick(event)
+  }
 
-    return (
-      <button
-        ref={ref}
-        className={`${baseStyles} ${isSelected ? appearanceStyles.selected : appearanceStyles.default} ${shapeStyles}`}
-        onClick={handleClick}
-        {...props}
-      >
-        {children}
-      </button>
-    )
-  })
-)
+  return (
+    <button
+      ref={ref}
+      className={`${baseStyles} ${isSelected ? appearanceStyles.selected : appearanceStyles.default} ${shapeStyles}`}
+      onClick={handleClick}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+})
 
 export { Button }
