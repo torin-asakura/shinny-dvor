c0a9d0facc4bef065d4fdc0a922af824eb987941
Viktoria Dragun <aliceinapple99@gmail.com>
2024-12-17 18:27:38 +0300

    feat(ui): rewrite select arrow component

---
+++ ui/select/src/arrow/arrow.component.tsx
@@ -0,0 +1,17 @@
+import type { ArrowContainerProps } from './arrow.interface.js'
+
+import React                        from 'react'
+
+import { memo }               from 'react'
+
+import { DropDownIcon }             from '@ui/icons'
+
+import { baseContainerStyles }      from './arrow.css.js'
+import { openStyles }               from './arrow.css.js'
+import { closedStyles }             from './arrow.css.js'
+
+export const Arrow = memo(({ isOpen }: ArrowContainerProps) => (
+  <div className={`${baseContainerStyles} ${isOpen ? openStyles : closedStyles}`}>
+    <DropDownIcon width={16} height={16} />
+  </div>
+))
