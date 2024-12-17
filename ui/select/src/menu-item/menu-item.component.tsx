99ef676a0237b0de53d8c3ff4de2c72394712271
Viktoria Dragun <aliceinapple99@gmail.com>
2024-12-17 18:35:51 +0300

    feat(ui): rewrite select menu-item component

--- ui/select/src/menu-item/menu-item.component.tsx
+++ ui/select/src/menu-item/menu-item.component.tsx
@@ -1,6 +1,6 @@
-import { forwardRef }    from 'react'
-import { memo }          from 'react'
 import React             from 'react'
+
+import { memo }    from 'react'
 
 import { Checkbox }      from '@ui/checkbox'
 import { Layout }        from '@ui/layout'
@@ -10,30 +10,31 @@ import { Text }          from '@ui/text'
 import { MenuItemProps } from './menu-item.interface.js'
 import { menuItemBase }  from './menu-item.css.js'
 
-export const MenuItem = memo(
-  forwardRef<HTMLLIElement, MenuItemProps>((
-    { children, selectedItems, addSelectedItem, removeSelectedItem, ...props },
-    ref
-  ) => {
-    const handleCheck = (newState: boolean): void => {
-      if (newState) addSelectedItem(children)
-      if (!newState) removeSelectedItem(children)
-    }
+export const MenuItem = memo(({
+  children,
+  selectedItems,
+  addSelectedItem,
+  removeSelectedItem,
+  ...props
+}: MenuItemProps) => {
+  const handleCheck = (newState: boolean): void => {
+    if (newState) addSelectedItem(children)
+    if (!newState) removeSelectedItem(children)
+  }
 
-    return (
-      <li ref={ref} className={menuItemBase} {...props}>
-        <Row>
-          <Layout flexBasis={16} flexShrink={0} />
-          <Checkbox active={selectedItems?.includes(children)} onCheck={handleCheck}>
-            <Layout>
-              <Text fontSize='$medium' color='$black'>
-                {children}
-              </Text>
-            </Layout>
-          </Checkbox>
-          <Layout flexBasis={16} flexShrink={0} />
-        </Row>
-      </li>
-    )
-  })
-)
+  return (
+    <li className={menuItemBase} {...props}>
+      <Row>
+        <Layout flexBasis={16} flexShrink={0} />
+        <Checkbox active={selectedItems?.includes(children)} onCheck={handleCheck}>
+          <Layout>
+            <Text fontSize='$medium' color='$black'>
+              {children}
+            </Text>
+          </Layout>
+        </Checkbox>
+        <Layout flexBasis={16} flexShrink={0} />
+      </Row>
+    </li>
+  )
+})
--- ui/select/src/menu/menu.component.tsx
+++ ui/select/src/menu/menu.component.tsx
@@ -1,10 +1,10 @@
-import { memo }                    from 'react'
 import { PropsWithChildren } from 'react'
-import React                      from 'react'
+import { memo }              from 'react'
+import React                 from 'react'
 
-import { menuBase }               from './menu.css.js'
-import { menuShape }              from './menu.css.js'
-import { menuAppearance }         from './menu.css.js'
+import { menuBase }          from './menu.css.js'
+import { menuShape }         from './menu.css.js'
+import { menuAppearance }    from './menu.css.js'
 
 export const Menu = memo(({ children, ...props }: PropsWithChildren) => (
   <div className={`${menuBase} ${menuShape} ${menuAppearance}`} {...props}>
