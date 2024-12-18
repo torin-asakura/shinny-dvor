af66171de9e5e9b8352457f83de28b23ecdfb7b2
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-18 14:34:41 +0300

    feat(ui): adapt to new library

---
+++ ui/text/src/ruble/ruble.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Ruble = () => {
+  return <>ruble</>
+}
+
+// const Span = styled.span(
+//   () => ({
+//     display: 'inline-flex',
+//   }),
+//   color
+// )
+//
+// export const Ruble = (): JSX.Element => createElement(Span, {}, '\u20BD')
--- ui/text/src/text-ellipsis/text-ellipsis.component.tsx
+++ ui/text/src/text-ellipsis/text-ellipsis.component.tsx
@@ -1,11 +1,17 @@
-import type { TextEllipsisProps } from './text-ellipsis.interface.js'
+import React from 'react'
 
-import styled                     from '@emotion/styled'
+export const TextEllipsis = () => {
+  return <>text-ellipsis</>
+}
 
-import { Text }                   from '../index.js'
-
-export const TextEllipsis = styled(Text)<TextEllipsisProps>(({ lineClamp }) => ({
-  display: '-webkit-box',
-  WebkitLineClamp: `${lineClamp}`,
-  WebkitBoxOrient: 'vertical',
-}))
+// import type { TextEllipsisProps } from './text-ellipsis.interface.js'
+//
+// import styled                     from '@emotion/styled'
+//
+// import { Text }                   from '../index.js'
+//
+// export const TextEllipsis = styled(Text)<TextEllipsisProps>(({ lineClamp }) => ({
+//   display: '-webkit-box',
+//   WebkitLineClamp: `${lineClamp}`,
+//   WebkitBoxOrient: 'vertical',
+// }))
