7d0e60d1394d8e59cbd62510417d5425b72c3461
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-18 22:49:08 +0300

    fix(ui): rm unused

--- ui/theme/src/index.ts
+++ ui/theme/src/index.ts
@@ -1,3 +1,2 @@
 export * from './theme.css.js'
 export * from './constants/index.js'
-export * from './button-colors/index.js'
--- ui/theme/src/theme.css.ts
+++ ui/theme/src/theme.css.ts
@@ -1,10 +1,9 @@
 import './global.css.js'
-import * as theme            from './theme/index.js'
 
 import { createGlobalTheme } from '@vanilla-extract/css'
+
+import * as theme            from './theme/index.js'
 
 export const GLOBAL_THEME_ID = '#default-theme'
 
 export const vars = createGlobalTheme(':root', theme)
-// export const vars = createGlobalTheme(GLOBAL_THEME_ID, theme)
-// export const buttonColorsVars = createGlobalTheme(BUTTON_COLORS_THEME_ID, buttonColorsTheme)
