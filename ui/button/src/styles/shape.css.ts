23d40872b4020a85d317154afa14a0f730ee93d3
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-18 13:16:46 +0300

    feat(ui): shape variants

--- ui/button/src/styles/shape.css.ts
+++ ui/button/src/styles/shape.css.ts
@@ -2,16 +2,45 @@ import { createShapeStyles } from '@atls-ui-parts/button'
 
 import { vars }              from '@ui/theme'
 
+const smallStyles = createShapeStyles({
+  size: vars.spaces['g40'],
+  rounding: vars.radii['large.huge'],
+  fontFamily: vars.fonts['primary'],
+  fontSize: vars.fontSizes['normal.reduced'],
+  fontWeight: vars.fontWeights['semiBold'],
+  paddingLeft: vars.spaces['g16'],
+  paddingRight: vars.spaces['g16'],
+})
+
+const ghostStyles = createShapeStyles({
+  size: 'auto',
+  paddingLeft: vars.spaces['zero'],
+  paddingRight: vars.spaces['zero'],
+})
+
 const commonStyles = createShapeStyles({
   size: vars.spaces['g48'],
   rounding: vars.radii['large.huge'],
   fontFamily: vars.fonts['primary'],
-  fontSize: vars.fontSizes['normal'],
+  fontSize: vars.fontSizes['normal.reduced'],
+  fontWeight: vars.fontWeights['semiBold'],
+  paddingLeft: vars.spaces['g24'],
+  paddingRight: vars.spaces['g24'],
+})
+
+const largeStyles = createShapeStyles({
+  size: vars.spaces['g56'],
+  rounding: vars.radii['large.huge'],
+  fontFamily: vars.fonts['primary'],
+  fontSize: vars.fontSizes['normal.reduced'],
   fontWeight: vars.fontWeights['semiBold'],
   paddingLeft: vars.spaces['g24'],
   paddingRight: vars.spaces['g24'],
 })
 
 export const shapeStyles = {
+  ghost: ghostStyles,
+  small: smallStyles,
   common: commonStyles,
+  large: largeStyles,
 }
