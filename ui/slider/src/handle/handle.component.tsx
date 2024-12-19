00ddb00cf676b4ed3214681be040bc8ca9e104b3
Viktoria Dragun <aliceinapple99@gmail.com>
2024-12-19 14:05:23 +0300

    refactor(ui): slider handle component

--- ui/slider/src/handle/handle.component.tsx
+++ ui/slider/src/handle/handle.component.tsx
@@ -1,21 +1,21 @@
-import type { FC }         from 'react'
-
 import React               from 'react'
+
+import { memo }      from 'react'
 
 import { DoubleArrowIcon } from '@ui/icons'
 import { Box }             from '@ui/layout'
 
-const Handle: FC = () => (
+const Handle: React.FC = memo(() => (
   <Box
     width={[44, 44, 52]}
     height={[44, 44, 52]}
-    borderRadius='normal'
-    backgroundColor='white'
+    borderRadius='$normal'
+    backgroundColor='$white'
     justifyContent='center'
     alignItems='center'
   >
     <DoubleArrowIcon width={24} height={24} />
   </Box>
-)
+))
 
 export { Handle }
