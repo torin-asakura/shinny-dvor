152492ece72a2318da6cd6e9677914394028c6be
Viktoria Dragun <aliceinapple99@gmail.com>
2024-12-19 14:06:12 +0300

    feat(ui): rewrite slider transition-container component

---
+++ ui/slider/src/transition-container/transition-container.component.tsx
@@ -0,0 +1,15 @@
+import React                             from 'react'
+
+import { memo }                    from 'react'
+
+import { PropsWithChildren } from 'react'
+
+import { BoxProps }                       from '@ui/layout'
+
+import { transitionContainerStyles }      from './transition-container.css.js'
+
+export const TransitionContainer = memo(({ children, ...props }: BoxProps) => (
+  <div className={`${transitionContainerStyles}`} {...props}>
+    {children}
+  </div>
+))
