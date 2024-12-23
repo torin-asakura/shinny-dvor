c1cfc9e0391840d6babd4c83597283f741d7f349
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-23 16:48:06 +0300

    refactor(ui): pump input

--- ui/input/src/input.component.tsx
+++ ui/input/src/input.component.tsx
@@ -1,53 +1,36 @@
-/* eslint-disable */
+import type { InputProps } from './input.interfaces.js'
 
-import type { ForwardRefRenderFunction } from 'react'
+import { RawInput }        from '@atls-ui-parts/input'
 
-import type { InputProps }               from './input.interfaces.js'
+import React               from 'react'
+import { forwardRef }      from 'react'
 
-import { RawInput as RawInputElement }   from '@atls-ui-parts/input'
-import { useChangeValue }                from '@atls-ui-parts/input'
-import styled                            from '@emotion/styled'
-import { forwardRef }                    from 'react'
-import React                             from 'react'
+import { Divider }         from '@ui/divider'
+import { Layout }          from '@ui/layout'
+import { Column }          from '@ui/layout'
 
-import { Divider }                       from '@ui/divider'
-import { Layout }                        from '@ui/layout'
-import { Column }                        from '@ui/layout'
+import { baseStyles }      from './input.css.js'
 
-import { baseStyles }                    from './input.styles.js'
-import { shapeStyles }                   from './input.styles.js'
-import { additionalAppearanceStyles }    from './input.styles.js'
-import { appearanceStyles }              from './input.styles.js'
+export const Input = forwardRef<HTMLInputElement, InputProps>((
+  { type, disabled, value, onChange, onChangeNative, ...props },
+  ref
+) => {
+  const onChangeValue = (e) => {
+    onChange(e.target.value)
+  }
 
-// export const InputElement = styled.div<any>(baseStyles, shapeStyles, appearanceStyles)
-// const RawInput = styled(RawInputElement)(additionalAppearanceStyles)
-
-export const Input = () => {
-  return <h1>input</h1>
-}
-
-// export const InputWithoutRef: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
-//   { value, size, type = 'text', disabled, onChange, onChangeNative, ...props },
-//   ref
-// ) => {
-//   const changeValue = useChangeValue(disabled, onChange, onChangeNative)
-//
-//   return (
-//     <Column width='100%'>
-//       <InputElement {...props} type={type} size={size}>
-//         <RawInput
-//           ref={ref}
-//           type={type}
-//           disabled={disabled}
-//           value={value}
-//           onChange={changeValue}
-//           {...props}
-//         />
-//       </InputElement>
-//       <Layout flexBasis={16} />
-//       <Divider backgroundColor={value !== '' ? 'primaryBlue' : 'gray'} />
-//     </Column>
-//   )
-// }
-//
-// export const Input = forwardRef<HTMLInputElement, InputProps>(InputWithoutRef)
+  return (
+    <Column width='100%' gap='$g16'>
+      <RawInput
+        ref={ref}
+        className={baseStyles}
+        type={type}
+        disabled={disabled}
+        value={value}
+        onChange={onChangeValue}
+        {...props}
+      />
+      <Divider color={value !== '' ? '$primaryBlue' : '$gray'} />
+    </Column>
+  )
+})
---
+++ ui/input/src/input.css.ts
@@ -0,0 +1,13 @@
+import { style } from '@vanilla-extract/css'
+
+import { vars }  from '@ui/theme'
+
+export const baseStyles = style({
+  fontFamily: vars.fonts.primary,
+  fontWeight: vars.fontWeights.normal,
+  height: vars.spaces['g32'],
+  fontSize: vars.fontSizes['normal.reduced'],
+  color: vars.colors.black,
+  backgroundColor: vars.colors.white,
+  lineHeight: vars.lineHeights.medium,
+})
