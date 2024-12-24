fa745cc6c877965388d24861694d77cde53cff89
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-24 12:51:13 +0300

    feat(ui): extend props

--- ui/text/src/text-ellipsis/text-ellipsis.component.tsx
+++ ui/text/src/text-ellipsis/text-ellipsis.component.tsx
@@ -1,11 +1,18 @@
-import { Text }              from '@atls-ui-parts/text'
-import { memo }              from 'react'
-import React                 from 'react'
+import type { PropsWithChildren } from 'react'
 
-import { TextEllipsisProps } from './text-ellipsis.interface.js'
-import { baseEllipsisStyle } from './text-ellipsis.css.js'
+import { Text }                   from '@atls-ui-parts/text'
 
-export const TextEllipsis = memo(({ lineClamp, children, ...props }: TextEllipsisProps) => {
+import React                      from 'react'
+import { memo }                   from 'react'
+
+import { TextEllipsisProps }      from './text-ellipsis.interface.js'
+import { baseEllipsisStyle }      from './text-ellipsis.css.js'
+
+export const TextEllipsis = memo(({
+  lineClamp,
+  children,
+  ...props
+}: PropsWithChildren<TextEllipsisProps>) => {
   const dynamicEllipsisStyle = {
     WebkitLineClamp: lineClamp.toString(),
   }
