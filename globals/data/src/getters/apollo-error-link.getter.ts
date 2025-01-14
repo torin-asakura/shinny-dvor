e5707028877c4582a6fdb66f6e034721ec31346d
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-14 17:02:44 +0300

    feat(globals): console debug message

--- globals/data/src/getters/apollo-error-link.getter.ts
+++ globals/data/src/getters/apollo-error-link.getter.ts
@@ -1,26 +1,29 @@
-import type { ApolloLink }    from '@apollo/client'
-import type { ErrorResponse } from '@apollo/client/link/error'
+import type { ApolloLink }            from '@apollo/client'
+import type { GraphQLFormattedError } from 'graphql'
 
-import { onError }            from '@apollo/client/link/error'
+import { onError }                    from '@apollo/client/link/error'
 
 export const getApolloErrorLink = (): ApolloLink => {
   const errorLink = onError((errorResponse) => {
-    const extendedErrorResponse = errorResponse as unknown as ErrorResponse & {
+    const extendedErrorResponse = errorResponse as unknown as {
+      graphQLErrors: Array<GraphQLFormattedError & { debugMessage: string }>
       cause?: Record<string, any>
     }
     const { graphQLErrors, cause } = extendedErrorResponse
 
     if (graphQLErrors) {
-      graphQLErrors.forEach(({ message, locations, path, extensions }) => {
-        console.debug(`[GraphQL error]: Message: ${message}`)
+      graphQLErrors.forEach(({ debugMessage, message, locations, path, extensions }) => {
+        console.debug(`DebugMessage: ${debugMessage}`)
+        console.debug(`Message: ${message}`)
         console.debug(`Location:`, locations)
         console.debug(`Path:`, path)
         console.debug(`Extenstions:`, extensions)
       })
     }
     if (cause) {
-      const { message, extensions, locations, path } = cause
-      console.debug(`Cause: ${message}`)
+      const { debugMessage, message, extensions, locations, path } = cause
+      console.debug(`DebugMessage: ${debugMessage}`)
+      console.debug(`Message: ${message}`)
       console.debug(`Location:`, locations)
       console.debug(`Path:`, path)
       console.debug(`Extenstions:`, extensions)
